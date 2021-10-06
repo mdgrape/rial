6d2b53206e89cac9fb42af22596377dc213db59e
ToruNiina <niina.toru.68u@gmail.com>
2021-10-06 19:29:21 +0900

    Add test code for sqrtSim

---
+++ src/test/scala/math/SqrtSimTest.scala
@@ -0,0 +1,123 @@
+
+//package rial.tests
+
+//import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//import scopt.OptionParser
+
+import scala.util.Random
+import scala.math._
+
+import spire.math.SafeLong
+import spire.math.Numeric
+import spire.implicits._
+
+import rial.math._
+import rial.util.ScalaUtil._
+import rial.arith._
+import rial.table._
+
+class SqrtSimTest extends FunSuite with BeforeAndAfterAllConfigMap {
+//class SqrtSimTest extends AnyFunSuite with BeforeAndAfterAllConfigMap {
+  var n = 1000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRealWithin( p : Double, spec: RealSpec, r : Random ) = {
+    val rD : Double = (r.nextDouble()-0.5)*p
+    val x = new RealGeneric(spec, rD)
+    new RealGeneric (spec, (x.value & (maskSL(spec.exW+1)<<spec.manW)) + SafeLong(BigInt(spec.manW, r)))
+  }
+
+  def generateRealFull ( spec: RealSpec, r : Random ) = {
+    new RealGeneric (spec, SafeLong(BigInt(spec.W, r)))
+  }
+
+  def errorLSB( x : RealGeneric, y : Double ) : Double = {
+    val err = x.toDouble - y
+    java.lang.Math.scalb(err, -x.exNorm+x.spec.manW)
+  }
+
+  def sqrtTest(t_even: FuncTableInt, t_odd: FuncTableInt, spec : RealSpec, n : Int, r : Random,
+    generatorStr : String, generator : ( (RealSpec, Random) => RealGeneric) ) = {
+    test(s"sqrt(x), format ${spec.toStringShort}, ${generatorStr}") {
+      var err1lsbPos = 0
+      var err1lsbNeg = 0
+      for(i <- 1 to n) {
+        val x    = generator(spec,r)
+        val x0   = x.toDouble
+        val z0   = math.sqrt(x0)
+        val z0r  = new RealGeneric(spec, z0)
+        val zi   = SqrtSim.sqrtSimGeneric( t_even, t_odd, x )
+        val zd   = zi.toDouble
+        val errf = zd - z0r.toDouble
+        val erri = errorLSB(zi, z0r.toDouble)
+        //println(f"${x.value.toLong}%x $z0 ${zi.toDouble}")
+        if (z0r.value != zi.value) {
+          val xsgn = bit(spec.W-1, x.value).toInt
+          val xexp = slice(spec.manW, spec.exW, x.value)
+          val xman = x.value & maskSL(spec.manW)
+
+          val ztestsgn = bit(spec.W-1, zi.value).toInt
+          val ztestexp = slice(spec.manW, spec.exW, zi.value)
+          val ztestman = zi.value & maskSL(spec.manW)
+
+          val zrefsgn = bit(spec.W-1, z0r.value).toInt
+          val zrefexp = slice(spec.manW, spec.exW, z0r.value)
+          val zrefman = z0r.value & maskSL(spec.manW)
+
+//           val zref = new RealGeneric(spec, zrefsgn, zrefexp.toInt, zrefman.toInt)
+//           val zrefd = zref.toDouble
+
+          println(f"gen = ${x0}")
+          println(f"ref = ${z0}, ref^2 = ${z0*z0}")
+//           println(f"ref'= ${zrefd}, ref'^2 = ${zrefd*zrefd}")
+          println(f"sim = ${zd}, sim^2 = ${zd*zd}")
+          println(f"test(${ztestsgn}|${ztestexp}(${ztestexp - spec.exBias})|${ztestman}(${ztestman.toLong}%x)) != ref(${zrefsgn}|${zrefexp}(${zrefexp - spec.exBias})|${zrefman}(${zrefman.toLong}%x)) = sqrt(x = ${xsgn}|${xexp}(${xexp - spec.exBias})|${xman}(${xman.toLong}%x))")
+        }
+        if (z0.isInfinity) {
+          assert(zd.isInfinity)
+        } else if (zd.isInfinite) {
+          assert(z0r.isInfinite)
+        } else if (x.isNaN) {
+          assert(zi.isNaN)
+        } else if (x.sgn == 1 && !x.isZero) {
+          assert(zi.isNaN)
+        } else {
+          if (erri.abs>=2.0) {
+            println(f"Error more than 2 LSB : ${x.toDouble}%14.7e : $z0%14.7e ${zi.toDouble}%14.7e $errf%14.7e $erri%f")
+          } else if (erri>=1.0) err1lsbPos+=1
+          else if (erri<= -1.0) err1lsbNeg+=1
+          assert(erri.abs<=1)
+        }
+        //println(f"$x%14.7e : $z0%14.7e $z%14.7e $errf%14.7e $erri%d")
+      }
+      println(f"N=$n%d : 1LSB errors positive $err1lsbPos%d / negative $err1lsbNeg%d")
+    }
+  }
+
+  val sqrtF32TableIEven = SqrtSim.sqrtTableGenerationEven( 2, 8, 23, 23+2 )
+  val sqrtF32TableIOdd  = SqrtSim.sqrtTableGenerationOdd ( 2, 8, 23, 23+2 )
+
+  sqrtTest(sqrtF32TableIEven, sqrtF32TableIOdd, RealSpec.Float32Spec, n, r,
+    "Test Within (-128,128)",generateRealWithin(128.0,_,_))
+  sqrtTest(sqrtF32TableIEven, sqrtF32TableIOdd, RealSpec.Float32Spec, n, r,
+    "Test All range",generateRealFull(_,_) )
+
+  val sqrtBF16TableIEven = SqrtSim.sqrtTableGenerationEven( 0, 7, 7, 7 )
+  val sqrtBF16TableIOdd  = SqrtSim.sqrtTableGenerationOdd ( 0, 7, 7, 7 )
+
+  sqrtTest(sqrtBF16TableIEven, sqrtBF16TableIOdd, RealSpec.BFloat16Spec, n, r,
+    "Test Within (-128,128)",generateRealWithin(128.0,_,_))
+  sqrtTest(sqrtBF16TableIEven, sqrtBF16TableIOdd, RealSpec.BFloat16Spec, n, r,
+    "Test All range",generateRealFull(_,_) )
+}
