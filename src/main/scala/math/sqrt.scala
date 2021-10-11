ed888cc4a6e35df490929872414d1a8b86c1243d
ToruNiina <niina.toru.68u@gmail.com>
2021-10-11 13:11:00 +0900

    WIP: implement fully-table sqrt

--- src/main/scala/math/sqrt.scala
+++ src/main/scala/math/sqrt.scala
@@ -60,7 +60,6 @@ class SqrtGeneric(
   val (xzero, xinf, xnan) = FloatChiselUtil.checkValue(spec, io.x)
   val xneg = if(spec.disableSign) {false.B} else {xsgn === 1.U(1.W)}
 //   printf("x = %d|%d(%d)|%d\n", xsgn, xex, xex-spec.exBias.U, xman)
-  // special values
 
   // sqrt never becomes inf nor underflows. 100 -> 10, 1/100 -> 1/10.
   // sqrt(-0) is not NaN, it is zero.
@@ -68,128 +67,123 @@ class SqrtGeneric(
   val zinf  = xinf
   val zzero = xzero
 
-  // 8   = 2^3   => 2sqrt(2)    = 2    * 1.414:  3(0011) ->  1(0001)
-  // 1/8 = 1/2^3 => 1/2sqrt(2)  = 2^-2 * 1.414: -3(1101) -> -2(1110)
-  // 32  = 2^5   => 2^2sqrt(2)  = 2^2  * 1.414:  5(0101) ->  2(0010)
-  // 1/32= 1/2^5 => 1/2^2sqrt(2)= 2^-3 * 1.414: -5(1011) -> -3(1101)
   val xExNobias = xex - exBias.U
   val zex0 = xExNobias(exW-1) ## xExNobias(exW-1, 1) // arith right shift
   val zEx  = Mux(zinf || znan, maskU(exW), Mux(zzero, 0.U(exW.W), zex0 + exBias.U))
   val zSgn = 0.U(1.W) // always positive.
 
+  // --------------------------------------------------------------------------
+  // mantissa calculation
+  //
+  // ex==even: 1.xxxx..x: [1, 2) -> 0.xxxx..x: [0, 1) ->    0 .xxxx..x: [0, 1)
+  // ex==odd: 1x.xxx..x0: [2, 4) -> x.xxx..x0: [0, 2) -> (1+x).xxx..x0: [1, 3)
+
+  val man_even = 0.U(2.W) ## xman(manW-1, 0)
+  val man_odd  = Mux(xman(manW-1)===1.U, 2.U(2.W), 1.U(2.W)) ## xman(manW-2, 0) ## 0.U(1.W)
+  val man = Mux(xExNobias(0) === 1.U(1.W), man_odd, man_even)
+  val emanW = manW + 2
+  // in tests, since this class is constructed from generated table in sqrtSim,
+  // adrW is already extended.
+
   val z0 = Wire(UInt(spec.W.W))
 
   if (order<=0) { // generate fixed-sized table. no calculation needed.
-    val adr = xman(manW-1, manW-adrW) // take MSBs as an address
+    val adr = man(emanW-1, emanW-adrW) // take MSBs as an address
 
-    // sqrt(1.m) (1.x * 2^4 -> sqrt(1.x) * 2^2)
-    val tbl_even = VecInit( (0L to (1L<<adrW)-1L).map(
+    val tbl = VecInit( (0L to (1L<<adrW)-1L).map(
       n => {
-        val x = 1.0 + n.toDouble / (1L<<adrW)  // value from 1 to 2
+        val x = 1.0 + (n.toDouble / (1L<<adrW)) * 4.0
         val y = round((math.sqrt(x)-1.0) * (1L<<manW))
         if (y >= (1L<<manW)) {
-          println("WARNING: mantissa reaches to 2")
+          if ((n.toDouble / (1L<<adrW)) < 0.75) {
+            println(f"WARNING: mantissa reaches to 2 with n = ${n.toBinaryString}/${(1L<<manW).toBinaryString}")
+          }
           maskL(manW).U(manW.W)
         } else {
           y.U(manW.W)
         }
       })
     )
-    // sqrt(1.m) * 1.414 (1.x * 2^5 -> sqrt(1.x) * sqrt(2) * 2^2)
-    val tbl_odd = VecInit( (0L to (1L<<adrW)-1L).map(
-      n => {
-        val x = 1.0 + n.toDouble / (1L<<adrW)  // value from 1 to 2
-        val y = round((math.sqrt(x * 2.0)-1.0) * (1L<<manW))
-        if (y >= (1L<<manW)) {
-          println("WARNING: mantissa reaches to 2")
-          maskL(manW).U(manW.W)
-        } else {
-          y.U(manW.W)
-        }
-      })
-    )
-    val zman = Mux(xExNobias(0) === 1.U(1.W), tbl_odd(adr), tbl_even(adr))
+    val zman = tbl(adr)
     val zeroFlush = zinf || zzero || znan
     z0 := zSgn ## zEx ## Mux(zeroFlush, znan ## 0.U((manW-1).W), zman)
   } else {
-    // Fractional part by Polynomial
-    val adr = ~xman(manW-1, manW-adrW)
-    val d   = Cat(xman(manW-adrW-1),~xman(manW-adrW-2,0)).asSInt
-//     printf("xman = %b(%d)\n", xman, xman)
-//     printf("d    = %b(%d)\n", d, d)
-//     printf("adr  = %b(%d)\n", adr, adr)
-
-    val eps = pow(2.0, -manW)
-    val tableDeven = new FuncTableDouble(x => 2.0 - math.sqrt(2.0 -     (x+eps)), order)
-    val tableDodd  = new FuncTableDouble(x => 2.0 - math.sqrt(4.0 - 2.0*(x+eps)), order)
-    tableDeven.addRange(0.0, 1.0, 1<<adrW) // gen table, dividing the range [0,1]
-    tableDodd .addRange(0.0, 1.0, 1<<adrW) // into 1<<adrW subregions
-    val tableIeven = new FuncTableInt( tableDeven, bp ) // convert float table
-    val tableIodd  = new FuncTableInt( tableDodd,  bp ) // into mantissa-Int
-    val (coeffTableEven, coeffWidthEven) = tableIeven.getVectorUnified(/*sign mode =*/1)
-    val (coeffTableOdd,  coeffWidthOdd ) = tableIodd .getVectorUnified(1)
-    // signMode=0 : always include sign bit
-    // signMode=1 : 2's complement and no sign bit (if possible)
-    // signMode=2 : absolute and no sign bit (if possible)
-
-    val coeffEven  = getSlices(coeffTableEven(adr), coeffWidthEven)
-    val coeffOdd   = getSlices(coeffTableOdd (adr), coeffWidthOdd)
-    val coeffSEven = coeffEven.map( x => x.zext )
-    val coeffSOdd  = coeffOdd .map( x => x.zext )
-
-    def hornerC( c: SInt, z: SInt, dx: SInt, enableRounding: Boolean = false ) : SInt = {
-      val zw   = z.getWidth
-      val dxBp = dx.getWidth-1
-      val dxw  = min(zw, dxBp)
-      val dxl  = dx(dxBp, dxBp-dxw).asSInt // ?
-      val prod = dxl * z
-      val prod_sft = dropLSB(dxw, prod)
-      val sum = c +& prod_sft // Extend for safe
-//       printf("----------------------------------------\n")
-//       printf("horner  : ci + dx * z; z is the current sum\n")
-//       printf("c       : %b(%d), W=%d\n", c              , c              , c              .getWidth.U)
-//       printf("zw      : %b(%d), W=%d\n", zw      .asUInt, zw      .asUInt, zw      .asUInt.getWidth.U)
-//       printf("dxBp    : %b(%d), W=%d\n", dxBp    .asUInt, dxBp    .asUInt, dxBp    .asUInt.getWidth.U)
-//       printf("dxw     : %b(%d), W=%d\n", dxw     .asUInt, dxw     .asUInt, dxw     .asUInt.getWidth.U)
-//       printf("dx      : %b(%d), W=%d\n", dx             , dx             , dx             .getWidth.U)
-//       printf("dxl     : %b(%d), W=%d\n", dxl            , dxl            , dxl            .getWidth.U)
-//       printf("z       : %b(%d), W=%d\n", z              , z              , z              .getWidth.U)
-//       printf("prod    : %b(%d), W=%d\n", prod           , prod           , prod           .getWidth.U)
-//       printf("prod_sft: %b(%d), W=%d\n", prod_sft       , prod_sft       , prod_sft       .getWidth.U)
-//       printf("sum     : %b(%d), W=%d\n", sum            , sum            , sum            .getWidth.U)
-      sum
-    }
-
-    def polynomialEvaluationC( c : Seq[SInt], dx : SInt, enableRounding: Boolean = false ) = {
-      val res = c.init.foldRight(c.last)( (c,z) => hornerC( c, z, dx, enableRounding ) )
-      res
-    }
-
-    val zodd  = polynomialEvaluationC( coeffSOdd,  d, enablePolynomialRounding ).asUInt
-    val zeven = polynomialEvaluationC( coeffSEven, d, enablePolynomialRounding ).asUInt
-
-    val res0 = Mux(xExNobias(0) === 1.U(1.W), zodd, zeven) // 2-sqrt(...)
-    val rres = res0 - (2.U << (manW + extraBits))          //  -sqrt(...)
-    val s0   = ~rres + 1.U - (1.U << (manW + extraBits))   //   sqrt(...)
-
-//     printf("Odd : %b(%d), width=%d\n", zodd , zodd , zodd .getWidth.U)
-//     printf("Even: %b(%d), width=%d\n", zeven, zeven, zeven.getWidth.U)
-//     printf("xEx : %b(%d), width=%d\n", xExNobias, xExNobias, xExNobias.getWidth.U)
-//     printf("res0: %b(%d), width=%d\n", res0,  res0, res0.getWidth.U)
-//     printf("rres: %b(%d), width=%d\n", rres,  rres, rres.getWidth.U)
-//     printf("s0  : %b(%d), width=%d\n", s0,    s0, s0.getWidth.U)
-
-    val zman  = dropLSB(extraBits, s0) + s0(extraBits-1)
-    val polynomialOvf = zman.head(2) === 1.U
-    val polynomialUdf = zman.head(1) === 1.U
-    val zeroFlush = zinf || zzero || polynomialUdf || znan
-    val zman_checkovf = Mux(polynomialOvf, Fill(manW,1.U(1.W)), zman(manW-1,0))
-
-//     printf("zman: %b(%d), width=%d\n", zman , zman , zman .getWidth.U)
-//     printf("xExNobias(0) = %b, zman.head(1) = %b, zman.head(2) = %b\n", xExNobias(0), zman.head(1), zman.head(2))
-//     printf("z0: %b|%b(%d)|%b(%d)\n", zSgn, zEx, zEx, zman_checkovf, zman_checkovf)
-
-    z0 := zSgn ## zEx ## Mux(zeroFlush, znan ## 0.U((manW-1).W), zman_checkovf)
+    z0 := 0.U
+//     // Fractional part by Polynomial
+//     val adr = ~xman(manW-1, manW-adrW)
+//     val d   = Cat(xman(manW-adrW-1),~xman(manW-adrW-2,0)).asSInt
+// //     printf("xman = %b(%d)\n", xman, xman)
+// //     printf("d    = %b(%d)\n", d, d)
+// //     printf("adr  = %b(%d)\n", adr, adr)
+//
+//     val eps = pow(2.0, -manW)
+//     val tableDeven = new FuncTableDouble(x => 2.0 - math.sqrt(2.0 -     (x+eps)), order)
+//     val tableDodd  = new FuncTableDouble(x => 2.0 - math.sqrt(4.0 - 2.0*(x+eps)), order)
+//     tableDeven.addRange(0.0, 1.0, 1<<adrW) // gen table, dividing the range [0,1]
+//     tableDodd .addRange(0.0, 1.0, 1<<adrW) // into 1<<adrW subregions
+//     val tableIeven = new FuncTableInt( tableDeven, bp ) // convert float table
+//     val tableIodd  = new FuncTableInt( tableDodd,  bp ) // into mantissa-Int
+//     val (coeffTableEven, coeffWidthEven) = tableIeven.getVectorUnified(/*sign mode =*/1)
+//     val (coeffTableOdd,  coeffWidthOdd ) = tableIodd .getVectorUnified(1)
+//     // signMode=0 : always include sign bit
+//     // signMode=1 : 2's complement and no sign bit (if possible)
+//     // signMode=2 : absolute and no sign bit (if possible)
+//
+//     val coeffEven  = getSlices(coeffTableEven(adr), coeffWidthEven)
+//     val coeffOdd   = getSlices(coeffTableOdd (adr), coeffWidthOdd)
+//     val coeffSEven = coeffEven.map( x => x.zext )
+//     val coeffSOdd  = coeffOdd .map( x => x.zext )
+//
+//     def hornerC( c: SInt, z: SInt, dx: SInt, enableRounding: Boolean = false ) : SInt = {
+//       val zw   = z.getWidth
+//       val dxBp = dx.getWidth-1
+//       val dxw  = min(zw, dxBp)
+//       val dxl  = dx(dxBp, dxBp-dxw).asSInt // ?
+//       val prod = dxl * z
+//       val prod_sft = dropLSB(dxw, prod)
+//       val sum = c +& prod_sft // Extend for safe
+// //       printf("----------------------------------------\n")
+// //       printf("horner  : ci + dx * z; z is the current sum\n")
+// //       printf("c       : %b(%d), W=%d\n", c              , c              , c              .getWidth.U)
+// //       printf("zw      : %b(%d), W=%d\n", zw      .asUInt, zw      .asUInt, zw      .asUInt.getWidth.U)
+// //       printf("dxBp    : %b(%d), W=%d\n", dxBp    .asUInt, dxBp    .asUInt, dxBp    .asUInt.getWidth.U)
+// //       printf("dxw     : %b(%d), W=%d\n", dxw     .asUInt, dxw     .asUInt, dxw     .asUInt.getWidth.U)
+// //       printf("dx      : %b(%d), W=%d\n", dx             , dx             , dx             .getWidth.U)
+// //       printf("dxl     : %b(%d), W=%d\n", dxl            , dxl            , dxl            .getWidth.U)
+// //       printf("z       : %b(%d), W=%d\n", z              , z              , z              .getWidth.U)
+// //       printf("prod    : %b(%d), W=%d\n", prod           , prod           , prod           .getWidth.U)
+// //       printf("prod_sft: %b(%d), W=%d\n", prod_sft       , prod_sft       , prod_sft       .getWidth.U)
+// //       printf("sum     : %b(%d), W=%d\n", sum            , sum            , sum            .getWidth.U)
+//       sum
+//     }
+//
+//     def polynomialEvaluationC( c : Seq[SInt], dx : SInt, enableRounding: Boolean = false ) = {
+//       val res = c.init.foldRight(c.last)( (c,z) => hornerC( c, z, dx, enableRounding ) )
+//       res
+//     }
+//
+//     val res0 = Mux(xExNobias(0) === 1.U(1.W), zodd, zeven) // 2-sqrt(...)
+//     val rres = res0 - (2.U << (manW + extraBits))          //  -sqrt(...)
+//     val s0   = ~rres + 1.U - (1.U << (manW + extraBits))   //   sqrt(...)
+//
+// //     printf("Odd : %b(%d), width=%d\n", zodd , zodd , zodd .getWidth.U)
+// //     printf("Even: %b(%d), width=%d\n", zeven, zeven, zeven.getWidth.U)
+// //     printf("xEx : %b(%d), width=%d\n", xExNobias, xExNobias, xExNobias.getWidth.U)
+// //     printf("res0: %b(%d), width=%d\n", res0,  res0, res0.getWidth.U)
+// //     printf("rres: %b(%d), width=%d\n", rres,  rres, rres.getWidth.U)
+// //     printf("s0  : %b(%d), width=%d\n", s0,    s0, s0.getWidth.U)
+//
+//     val zman  = dropLSB(extraBits, s0) + s0(extraBits-1)
+//     val polynomialOvf = zman.head(2) === 1.U
+//     val polynomialUdf = zman.head(1) === 1.U
+//     val zeroFlush = zinf || zzero || polynomialUdf || znan
+//     val zman_checkovf = Mux(polynomialOvf, Fill(manW,1.U(1.W)), zman(manW-1,0))
+//
+// //     printf("zman: %b(%d), width=%d\n", zman , zman , zman .getWidth.U)
+// //     printf("xExNobias(0) = %b, zman.head(1) = %b, zman.head(2) = %b\n", xExNobias(0), zman.head(1), zman.head(2))
+// //     printf("z0: %b|%b(%d)|%b(%d)\n", zSgn, zEx, zEx, zman_checkovf, zman_checkovf)
+//
+//     z0 := zSgn ## zEx ## Mux(zeroFlush, znan ## 0.U((manW-1).W), zman_checkovf)
   }
 
   io.z   := ShiftRegister(z0, nStage)
