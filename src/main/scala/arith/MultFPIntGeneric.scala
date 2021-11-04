c1fc4bcad064da54d427c1656a38061357abb992
ToruNiina <niina.toru.68u@gmail.com>
2021-11-04 14:56:45 +0900

    Add FP * Int multiplication

---
+++ src/main/scala/arith/MultFPIntGeneric.scala
@@ -0,0 +1,130 @@
+//% @file MultFPIntGeneric.scala
+//
+// multiply FP and int and return FP
+// Copyright (C) Toru Niina RIKEN BDR 2021
+//
+package rial.arith
+
+import scala.language.reflectiveCalls
+import scala.math._
+import chisel3._
+import chisel3.util._
+import rial.table._
+import rial.util._
+import rial.util.RialChiselUtil._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig._
+
+class MultFPIntGeneric(
+  xSpec : RealSpec, yWidth : Int, ySigned : Boolean, zSpec : RealSpec,
+  roundSpec : RoundSpec, // Rounding spec
+  stage : PipelineStageConfig
+) extends Module {
+
+  val nStage = stage.total
+
+  def getParam() = { (xSpec, yWidth, ySigned, zSpec, roundSpec, nStage) }
+
+  def getStage() = nStage
+
+  val io = IO(iodef = new Bundle {
+    val x   = Input (UInt(xSpec.W.W))
+    val y   = Input (UInt(yWidth.W))
+    val z   = Output(UInt(zSpec.W.W))
+  })
+  printf("x = %b\n", io.x)
+
+  val (xsgn,  xex,  xman) = FloatChiselUtil.decomposeWithLeading1(xSpec, io.x)
+  val (xzero, xinf, xnan) = FloatChiselUtil.checkValue(xSpec, io.x)
+
+  printf("x = %b|%b(%d)|%b\n", xsgn, xex, xex-xSpec.exBias.U, xman)
+
+  val ysgn = Wire(UInt(1.W))
+  val yval = Wire(UInt(yWidth.W))
+
+  if (ySigned) {
+    ysgn := io.y(yWidth-1)
+    yval := Mux(ysgn === 0.U(1.W), io.y, ~io.y + 1.U) // abs
+  } else {
+    ysgn := 0.U(1.W)
+    yval := io.y
+  }
+
+  val yLength = (yWidth-1).U - PriorityEncoder(Reverse(yval))
+  val yman    = (yval << ((yWidth-1).U - yLength))(yWidth, 0)
+  val yex     = yLength
+  printf("y = %b(%d)\n", io.y, io.y)
+  printf("yWidth  = %d\n", yWidth.U)
+  printf("yLength = %d\n", yLength)
+  printf("y = %b|%b|%b\n", ysgn, yex, yman)
+
+  val zzero = xzero.asBool || yval === 0.U
+  val zinf  = xinf.asBool
+  val znan  = xnan.asBool
+  val zsgn0 = (xsgn ^ ysgn)
+  //printf("%x %x\n",  io.x, io.y)
+  //printf("%b %b %b\n",  zzero, zinf, znan)
+
+  printf("zzero, zinf, znan, zsgn0 = %b,%b,%b,%b\n",zzero, zinf, znan, zsgn0)
+
+  //----------------------------------------------------------------------
+  // Mantissa
+
+  val prod      = xman * yman
+
+  printf("xman * yman = %b\n", prod)
+
+  val bp        = xSpec.manW+yWidth-1
+  val roundBits = bp-zSpec.manW
+  val moreThan2 = prod(bp+1)
+  val sticky    = prod(roundBits-2,0).orR | (moreThan2 & prod(roundBits-1))
+  val round     = Mux(moreThan2, prod(roundBits), prod(roundBits-1))
+  val prodShift = Mux(moreThan2, prod(bp, roundBits+1), prod(bp-1, roundBits))
+  val lsb       = prodShift(0)
+  // RoundSpec : truncate/ceil always towards 0/infinite
+  val inc = FloatChiselUtil.roundIncBySpec(roundSpec, lsb, round, sticky)
+  val prodRound = prodShift +& inc
+  val moreThan2AfterRound = prodRound(zSpec.manW)
+  val resMan    = prodRound(zSpec.manW-1,0)
+
+  //----------------------------------------------------------------------
+  // Exponent
+
+  val maxEx = ( maskI(xSpec.exW)-1-xSpec.exBias + yWidth - 1 + zSpec.exBias )
+  val minEx = 1-xSpec.exBias+zSpec.exBias
+
+  val exW0 = max( log2Up(maxEx+1), log2Up(abs(minEx)+1))
+  val exW  = if (minEx<0) {exW0+1} else {exW0}
+
+  val exAdd = (-xSpec.exBias + zSpec.exBias).S(exW.W).asUInt
+  val ex0   = xex.pad(exW) + yex.pad(exW) + exAdd
+
+  val exInc = ex0 + (moreThan2 | moreThan2AfterRound)
+
+  val exZero = !exInc.orR.asBool // needed?
+  val exNeg  = (minEx<0).B && (exInc(exW-1)=/=0.U)
+  val exZN   = exZero || exNeg || zzero
+  val exInf  = zinf || exInc(zSpec.exW-1,0).andR | exInc(exW-1, zSpec.exW).orR
+
+  val zex = Mux(exZN,         0.U(zSpec.exW),
+            Mux(exInf | znan, Fill(zSpec.exW,1.U(1.W)),
+                              exInc(zSpec.exW-1,0)))
+
+  // Final mantissa
+  val zman = Mux(exZN || exInf || znan, znan ## 0.U((zSpec.manW-1).W), resMan)
+  val zsgn = ( !(exZN || znan) ) && zsgn0.asBool
+  //printf("%x\n", zman)
+
+  val z0 = if (zSpec.disableSign) (zex ## zman) else (zsgn ## zex ## zman)
+
+  printf("z = %b|%b(%d)|%b\n", zsgn, zex, zex-zSpec.exBias.U, zman)
+
+  io.z   := ShiftRegister(z0, nStage)
+  //printf("x=%x y=%x z=%x\n", io.x, io.y, io.z)
+}
+
+class MultFPInt64( stage : PipelineStageConfig )
+    extends MultFPIntGeneric( RealSpec.Float64Spec, 64, true, RealSpec.Float64Spec,
+      RoundSpec.roundToEven, stage) {
+}
+
---
+++ src/test/scala/arith/MultFPIntTest.scala
@@ -0,0 +1,119 @@
+
+import org.scalatest._
+
+import chisel3._
+import chisel3.experimental.BundleLiterals._
+import chiseltest._
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+import rial.math._
+import rial.arith._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig
+
+import scala.util.Random
+import scala.math._
+import scala.collection.mutable.Queue
+import scala.language.reflectiveCalls
+
+import spire.math.SafeLong
+import spire.implicits._
+
+//
+// Testing Pow2F32 using ChiselTest
+//
+
+class MultFPIntTest extends FlatSpec
+    with ChiselScalatestTester with Matchers with BeforeAndAfterAllConfigMap {
+
+  behavior of "Test x:Float * y:Int"
+
+  var n = 1000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRealWithin( p : Double, spec: RealSpec, r : Random ) = {
+    val rD : Double = (r.nextDouble()-0.5)*p
+    val x = new RealGeneric(spec, rD)
+    new RealGeneric (spec, (x.value & (maskSL(spec.exW+1)<<spec.manW)) + SafeLong(BigInt(spec.manW, r)))
+  }
+
+  def generateRealFull ( spec: RealSpec, r : Random ) = {
+    new RealGeneric (spec, SafeLong(BigInt(spec.W, r)))
+  }
+
+  def generateLongWithin( width : Int, signed : Boolean, r : Random ) = {
+    if (signed) {
+      val sgn = if ( r.nextBoolean() ) { -1 } else { 1 }
+      val rI : Long = r.nextLong() & maskL(width-1)
+      rI * sgn
+    } else {
+      val rI : Long = r.nextLong() & maskL(width)
+      rI
+    }
+  }
+
+  def multTest(xSpec : RealSpec, yWidth : Int, ySigned : Boolean, zSpec : RealSpec, roundSpec : RoundSpec,
+    n : Int, stage : PipelineStageConfig ) = {
+      test(new MultFPIntGeneric( xSpec, yWidth, ySigned, zSpec, roundSpec, stage )) { c =>
+        {
+          var q  = new Queue[(BigInt,BigInt,BigInt)]
+          val nstage = c.getStage
+          for (gen <- List( ("Test Within (-128,128)",generateRealWithin(128.0,_,_)),
+                            ("Test All range",        generateRealFull(_,_)) ) ) {
+            println(gen._1)
+            for(i <- 1 to n+nstage) {
+              val xr = gen._2(xSpec, r)
+              val yr = generateLongWithin(yWidth, ySigned, r)
+              val xi = xr.value.toBigInt
+              val yi = yr.toBigInt
+              val zr = new RealGeneric(zSpec, xr.toDouble * yr) // TODO write simulator
+              val z0i= zr.value.toBigInt
+              println(f"x = ${xi}(${xr.toDouble}), y = ${yr}, z = ${zr.value}")
+              q += ((xi,yi,z0i))
+              c.io.x.poke(xi.U(xSpec.W.W))
+              if(ySigned) {
+                c.io.y.poke((yi & maskL(yWidth)).U(yWidth.W))
+              } else {
+                c.io.y.poke(yi.U(yWidth.W))
+              }
+              val zi = c.io.z.peek.litValue.toBigInt
+              c.clock.step(1)
+              if (i > nstage) {
+                val (xid,yid,z0d) = q.dequeue
+
+                val zisgn  = bit(zSpec.W-1, zi).toInt
+                val ziexp  = slice(zSpec.manW, zSpec.exW, zi)
+                val ziman  = zi & maskSL(zSpec.manW)
+                val z0dsgn = bit(zSpec.W-1, z0d).toInt
+                val z0dexp = slice(zSpec.manW, zSpec.exW, z0d)
+                val z0dman = z0d & maskSL(zSpec.manW)
+                assert(zisgn == z0dsgn && ziexp == z0dexp && ziman == z0dman,
+                  f"test(${zisgn}|${ziexp}|${ziman}) != ref(${z0dsgn}|${z0dexp}|${z0dman})")
+              }
+            }
+            q.clear
+          }
+        }
+      }
+  }
+
+  it should f"Multiplier Float * UInt8 with pipereg 0" in {
+    multTest( RealSpec.Float32Spec, 8, false, RealSpec.Float32Spec,
+      RoundSpec.roundToEven, n, PipelineStageConfig.none())
+  }
+  it should f"Multiplier Float * SInt8 with pipereg 0" in {
+    multTest( RealSpec.Float32Spec, 8, true, RealSpec.Float32Spec,
+      RoundSpec.roundToEven, n, PipelineStageConfig.none())
+  }
+}
+
