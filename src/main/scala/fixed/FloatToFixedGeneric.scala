97e0570c28cc161d43532d5e85f4eb9bea729242
ToruNiina <niina.toru.68u@gmail.com>
2021-11-16 18:28:06 +0900

    Add float to fixed and its test

---
+++ src/main/scala/fixed/FloatToFixedGeneric.scala
@@ -0,0 +1,86 @@
+//% @file FloatToFixedGeneric.scala
+//
+// Fixed point -> floating point
+// Copyright (C) Toru Niina RIKEN BDR 2021
+//
+package rial.fixed
+
+import scala.language.reflectiveCalls
+import scala.math._
+import chisel3._
+import chisel3.util._
+import rial.arith._
+import rial.fixed._
+import rial.util._
+import rial.util.RialChiselUtil._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig._
+
+class FloatToFixedGeneric(
+  xSpec : RealSpec, zSpec : FixedSpec, // Input / Output floating spec
+  roundSpec : RoundSpec, // Rounding spec
+  stage : PipelineStageConfig
+) extends Module {
+
+  val nStage = stage.total
+
+  def getParam() = { (xSpec, zSpec, roundSpec, nStage) }
+
+  def getStage() = nStage
+
+  val io = IO(iodef = new Bundle {
+    val x = Input (UInt(xSpec.W.W))
+    val z = Output(UInt(zSpec.W.W))
+  })
+
+  val (xsgn,  xex,  xmanW1) = FloatChiselUtil.decomposeWithLeading1(xSpec, io.x)
+  val (xzero, xinf, xnan)   = FloatChiselUtil.checkValue(xSpec, io.x)
+
+  val zTmpWidth = xSpec.manW + 1 + zSpec.W
+  val zTmp      = Cat(xmanW1, Fill(zSpec.W, 0.U(1.W)))
+  val zTmpEx    = zSpec.intW - 1
+
+  val zTmpShiftBits = log2Up(zSpec.W)
+  val zTmpShift     = ((zSpec.intW-1 + xSpec.exBias).U - xex)(zTmpShiftBits-1, 0)
+  val zTmpShifted   = zTmp >> zTmpShift
+
+  val zTmpLSB    = zTmpShifted(xSpec.manW+1).asBool
+  val zTmpRound  = zTmpShifted(xSpec.manW  ).asBool
+  val zTmpSticky = zTmpShifted(xSpec.manW-1, 0).orR
+  val zTmpInc    = FloatChiselUtil.roundIncBySpec(roundSpec, zTmpLSB, zTmpRound, zTmpSticky)
+
+  val zAbs = zTmpShifted(zSpec.W + xSpec.manW, xSpec.manW+1)
+  val z    = Mux(xsgn === 0.U, zAbs, ~zAbs + 1.U)
+
+  // -----------------------------------------------------------------------
+
+  val zPosInf = if(zSpec.signed) {
+    ((1L<<(zSpec.W-1))-1L).U(zSpec.W.W)
+  } else {
+    ((1L<<zSpec.W)-1L).U(zSpec.W.W)
+  }
+  val zNegInf = if(zSpec.signed) {
+    (1L<<(zSpec.W-1L)).U(zSpec.W.W) // ?
+  } else {
+    0.U(zSpec.W.W)
+  }
+
+  val zExMax =   zSpec.intW  - 1
+  val zExMin = -(zSpec.fracW + 1)
+  val zExMaxBiased = xSpec.exBias + zExMax
+  val zExMinBiased = max(0, xSpec.exBias + zExMin)
+
+  val isZInf    = xinf  || zExMaxBiased.U < xex
+  val isZZero   = xzero || xex < zExMinBiased.U
+  val isZPosInf = isZInf && (xsgn === 0.U)
+  val isZNegInf = isZInf && (xsgn === 1.U)
+
+  val z0 = MuxCase(z, Seq(
+    xnan      -> zPosInf,
+    isZPosInf -> zPosInf,
+    isZNegInf -> zNegInf,
+    isZZero   -> 0.U(zSpec.W.W)
+    ))
+
+  io.z := ShiftRegister(z0, nStage)
+}
---
+++ src/test/scala/fixed/FloatToFixedTest.scala
@@ -0,0 +1,90 @@
+import org.scalatest._
+
+import chisel3._
+import chisel3.experimental.BundleLiterals._
+import chiseltest._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+import rial.arith._
+import rial.fixed._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig
+
+import scala.util.Random
+import scala.math._
+import scala.collection.mutable.Queue
+import scala.language.reflectiveCalls
+
+import spire.math.SafeLong
+import spire.implicits._
+
+//
+// Testing Float -> Fixed using ChiselTest
+//
+
+class FloatToFixedTest extends FlatSpec
+    with ChiselScalatestTester with Matchers with BeforeAndAfterAllConfigMap {
+
+  behavior of "Test float -> fixed"
+
+  var n = 10000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRealWithin( p : Double, spec: RealSpec, r : Random ) = {
+    val rD : Double = (r.nextDouble()-0.5)*p
+    val x = new RealGeneric(spec, rD)
+    new RealGeneric (spec, (x.value & (maskSL(spec.exW+1)<<spec.manW)) + SafeLong(BigInt(spec.manW, r)))
+  }
+
+  def convTest(xSpec : RealSpec, zSpec : FixedSpec, roundSpec : RoundSpec,
+    n : Int, stage : PipelineStageConfig, range : Double ) = {
+      test(new FloatToFixedGeneric( xSpec, zSpec, roundSpec, stage )) { c =>
+        {
+          var q  = new Queue[(BigInt,BigInt)]
+          val delta  = pow(2.0, -zSpec.fracW)
+          val nstage = c.getStage
+          val gen = generateRealWithin(range,_,_)
+          println(f"Test Within (-${range}, ${range})")
+          for(i <- 1 to n+nstage) {
+            val x0 = gen(xSpec, r).toDouble
+            val z  = (x0 / delta).toLong
+            val x  = new RealGeneric(xSpec, z * delta)
+
+            val xi = x.value.toBigInt
+            val ziref = if(z >= 0) { z.toBigInt } else { (z & maskL(zSpec.W)).toBigInt }
+
+            q += ((xi, ziref))
+            c.io.x.poke(xi.U(xSpec.W.W))
+            val zi = c.io.z.peek.litValue.toBigInt
+            c.clock.step(1)
+            if (i > nstage) {
+              val (xid,zid) = q.dequeue
+              assert(zi == zid, f"x=${xid.toLong.toBinaryString}(${new RealGeneric(xSpec, xid).toDouble}) actual(${zi.toLong.toBinaryString}) != expect(${zid.toLong.toBinaryString})")
+            }
+          }
+          q.clear
+        }
+      }
+  }
+
+  it should f"Converter signed Q24 to Float with pipereg 0" in {
+    convTest( RealSpec.Float32Spec, new FixedSpec(32, 24, true, true),
+      RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-24-1))
+  }
+  it should f"Converter signed Q30 to Float with pipereg 0" in {
+    convTest( RealSpec.Float32Spec, new FixedSpec(32, 30, true, true),
+      RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-30-1))
+  }
+  it should f"Converter signed Q16 to Float with pipereg 0" in {
+    convTest( RealSpec.Float32Spec, new FixedSpec(32, 16, true, true),
+      RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-16-1))
+  }
+}
+
