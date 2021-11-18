9af5726d7933a32392ddc14661a6af7722706d26
ToruNiina <niina.toru.68u@gmail.com>
2021-11-18 21:56:26 +0900

    Add scaled fixed to float generic

---
+++ src/main/scala/fixed/ScaledFixedToFloatGeneric.scala
@@ -0,0 +1,123 @@
+//% @file ScaledFixedToFloatGeneric.scala
+//
+// Fixed point * floating point -> floating point
+// Copyright (C) Toru Niina RIKEN BDR 2021
+//
+package rial.fixed
+
+import scala.language.reflectiveCalls
+import scala.math._
+import chisel3._
+import chisel3.util._
+import rial.arith._
+import rial.fixed._
+import rial.util._
+import rial.util.RialChiselUtil._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig._
+
+class ScaledFixedToFloatGeneric(
+  xSpec : FixedSpec, ySpec : RealSpec, zSpec : RealSpec, // Input / Output floating spec
+  roundSpec : RoundSpec, // Rounding spec
+  stage : PipelineStageConfig
+) extends Module {
+
+  val nStage = stage.total
+
+  def getParam() = { (xSpec, ySpec, zSpec, roundSpec, nStage) }
+
+  def getStage() = nStage
+
+  val io = IO(iodef = new Bundle {
+    val x = Input (UInt(xSpec.W.W))
+    val y = Input (UInt(ySpec.W.W))
+    val z = Output(UInt(zSpec.W.W))
+  })
+
+  val (ysgn,  yex,  ymanW1) = FloatChiselUtil.decomposeWithLeading1(ySpec, io.y)
+  val (yzero, yinf, ynan)   = FloatChiselUtil.checkValue(ySpec, io.y)
+
+  val xzero = ~io.x.orR
+  val xsgn  = Wire(UInt(1.W))
+  val xabs  = Wire(UInt(xSpec.W.W))
+  if (xSpec.signed) {
+    xsgn := io.x(xSpec.W-1)
+    xabs := Mux(io.x(xSpec.W-1), ~io.x + 1.U, io.x)
+  } else {
+    xsgn := 0.U(1.W)
+    xabs := io.x
+  }
+
+  val xclz = PriorityEncoder(Reverse(xabs))
+  val xShifted = (xabs << xclz)(xSpec.W-1, 0)
+
+  // -------------------------------------------------------------------------
+  // mantissa
+
+  val prod = xShifted * ymanW1
+  val prodWidth = xSpec.W + ySpec.manW+1
+  val prodMoreThan2 = prod(prodWidth - 1)
+  assert(prodWidth == prod.getWidth)
+
+  //     decimal point
+  //     |
+  //     v  xSpec.W + y.manW + 1 = prodWidth
+  // .---+------------------------------.
+  // | | |          L|R|S.....          |
+  //  ^'-------------' '----------------'
+  //  1 :   z.manW    :     sticky
+  //  : :             :
+  //  : :             roundBit = prodWidth - 1 - manW
+  //  : prodWidth - 2
+  //  prodWidth - 1
+
+  val roundBit = prodWidth - zSpec.manW - 1
+
+  val lsb    = Mux(prodMoreThan2, prod(roundBit),   prod(roundBit-1))
+  val round  = Mux(prodMoreThan2, prod(roundBit-1), prod(roundBit-2))
+  val sticky = (prodMoreThan2 & prod(roundBit-2)) | prod(roundBit-3, 0).orR
+
+  val man0 = Mux(prodMoreThan2, prod(prodWidth-2, roundBit),
+                                prod(prodWidth-3, roundBit-1))
+  val inc = FloatChiselUtil.roundIncBySpec(roundSpec, lsb.asBool, round.asBool, sticky)
+  val manRounded = man0 +& inc.asUInt
+
+  val moreThan2AfterRound = manRounded(zSpec.manW)
+
+  // -------------------------------------------------------------------------
+  // exponent
+
+  val maxEx = xSpec.intW-1 + ySpec.exMax
+  val minEx = -xSpec.fracW + ySpec.exMin
+
+  val zExInc = (prodMoreThan2 | moreThan2AfterRound).asUInt
+  val yExNobias = yex.zext - ySpec.exBias.S
+  val xExNobias = (xSpec.intW - 1).S - xclz.zext
+  val zExNobias = yExNobias +& xExNobias + zExInc.zext
+  val zEx0      = zExNobias + zSpec.exBias.S
+
+  val znan  = ynan || (yinf && xzero)
+  val zinf  = (zSpec.exMax.S < zExNobias) || yinf
+  val zzero = yzero || xzero || zEx0 <= 0.S
+
+  val zEx = Mux(zzero,       0.U(zSpec.exW),
+            Mux(zinf | znan, Fill(zSpec.exW, 1.U(1.W)),
+                             zEx0(zSpec.exW-1, 0)))
+
+  // -------------------------------------------------------------------------
+  // merge
+
+  val zMan = Mux(zzero,       0.U(zSpec.manW),
+             Mux(zinf | znan, Cat(znan.asUInt, Fill(zSpec.manW-1, 0.U(1.W))),
+                              manRounded(zSpec.manW-1, 0)))
+
+  val z0 = if (zSpec.disableSign) {
+    Cat(zEx, zMan)
+  } else {
+    val zSgn = xsgn ^ ysgn
+    Cat(zSgn, zEx, zMan)
+  }
+//   printf("z0    = %b(w=%d)\n", z0 , z0.getWidth.U)
+
+  io.z := ShiftRegister(z0, nStage)
+}
---
+++ src/test/scala/fixed/ScaledFixedToFloatTest.scala
@@ -0,0 +1,106 @@
+import org.scalatest._
+
+import chisel3._
+import chisel3.experimental.BundleLiterals._
+import chiseltest._
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+import rial.arith._
+import rial.fixed._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig
+
+import scala.util.Random
+import scala.math._
+import scala.collection.mutable.Queue
+import scala.language.reflectiveCalls
+
+import spire.math.SafeLong
+import spire.implicits._
+
+//
+// Testing Fixed -> Float using ChiselTest
+//
+
+class ScaledFixedToFloatTest extends FlatSpec
+    with ChiselScalatestTester with Matchers with BeforeAndAfterAllConfigMap {
+
+  behavior of "Test fixed -> float"
+
+  var n = 10000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRealWithin( p : Double, spec: RealSpec, r : Random ) = {
+    val rD : Double = (r.nextDouble()-0.5)*p
+    val x = new RealGeneric(spec, rD)
+    new RealGeneric (spec, (x.value & (maskSL(spec.exW+1)<<spec.manW)) + SafeLong(BigInt(spec.manW, r)))
+  }
+
+  def convTest(xSpec : FixedSpec, ySpec : RealSpec, zSpec : RealSpec, roundSpec : RoundSpec,
+    n : Int, stage : PipelineStageConfig, xrange : Double, yrange : Double ) = {
+      test(new ScaledFixedToFloatGeneric( xSpec, ySpec, zSpec, roundSpec, stage )) { c =>
+        {
+          var q  = new Queue[(BigInt, BigInt, BigInt)]
+          val delta  = pow(2.0, -xSpec.fracW)
+          val nstage = c.getStage
+          val gen_x = generateRealWithin(xrange,_,_)
+          val gen_y = generateRealWithin(yrange,_,_)
+          println(f"Test x within (-${xrange}, ${xrange})")
+          for(i <- 1 to n+nstage) {
+            val x0 = gen_x(zSpec, r).toDouble / delta
+            val xi = if(x0 >= 0) {
+              x0.toBigInt
+            } else {
+              (x0.toLong & maskL(xSpec.W)).toBigInt
+            }
+            val y  = gen_y(ySpec, r)
+            val yi = y.value.toBigInt
+            val z  = new RealGeneric(zSpec, ((x0.toLong * delta) * y.toDouble))
+            val zi0 = (z.value.toLong & maskL(zSpec.W)).toBigInt
+
+            println(f"x = ${xi}(${x0 * delta}), y = ${y.toDouble}, z = ${z.toDouble}")
+
+            q += ((xi,yi,zi0))
+            c.io.x.poke(xi.U(xSpec.W.W))
+            c.io.y.poke(yi.U(xSpec.W.W))
+            val zi = c.io.z.peek.litValue.toBigInt
+            c.clock.step(1)
+            if (i > nstage) {
+              val (xid,yid,zid) = q.dequeue
+
+              val zisgn = bit(zSpec.W-1,  zi)
+              val ziex  = slice(zSpec.manW, zSpec.exW,  zi)
+              val ziman = slice(0,          zSpec.manW, zi)
+              val zdsgn = bit(zSpec.W-1,  zid)
+              val zdex  = slice(zSpec.manW, zSpec.exW,  zid)
+              val zdman = slice(0,          zSpec.manW, zid)
+
+              assert(zi == zid, f"x=${xid.toLong.toBinaryString} ${zisgn}|${ziex}|${ziman.toLong.toBinaryString} != ${zdsgn}|${zdex}|${zdman.toLong.toBinaryString}")
+            }
+          }
+          q.clear
+        }
+      }
+  }
+
+  it should f"Converter signed Q24 to Float with pipereg 0" in {
+    convTest( new FixedSpec(32, 24, true, true), RealSpec.Float32Spec, RealSpec.Float32Spec,
+      RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-24-1), pow(2.0, 128.0))
+  }
+//   it should f"Converter signed Q30 to Float with pipereg 0" in {
+//     convTest( new FixedSpec(32, 30, true, true), RealSpec.Float32Spec, RealSpec.Float32Spec,
+//       RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-30-1), pow(2.0, 128.0))
+//   }
+//   it should f"Converter signed Q16 to Float with pipereg 0" in {
+//     convTest( new FixedSpec(32, 16, true, true), RealSpec.Float32Spec, RealSpec.Float32Spec,
+//       RoundSpec.roundToEven, n, PipelineStageConfig.none(), pow(2.0, 32-16-1), pow(2.0, 128.0))
+//   }
+}
+
