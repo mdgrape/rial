ede394eb0c40308a12cc72c0c007154dc00caf3c
ToruNiina <niina.toru.68u@gmail.com>
2021-12-02 18:36:58 +0900

    Simplify io connection in sqrt using Bundles

--- src/main/scala/mathfunc/sqrt.scala
+++ src/main/scala/mathfunc/sqrt.scala
@@ -19,6 +19,14 @@ import rial.arith.FloatChiselUtil
 
 import rial.mathfunc._
 
+// -------------------------------------------------------------------------
+//  _ __  _ __ ___ _ __  _ __ ___   ___ ___  ___ ___
+// | '_ \| '__/ _ \ '_ \| '__/ _ \ / __/ _ \/ __/ __|
+// | |_) | | |  __/ |_) | | | (_) | (_|  __/\__ \__ \
+// | .__/|_|  \___| .__/|_|  \___/ \___\___||___/___/
+// |_|            |_|
+// -------------------------------------------------------------------------
+
 // sqrt(x): floating => floating
 // - if x < 0, returns 0.
 class SqrtPreProcess(
@@ -41,6 +49,14 @@ class SqrtPreProcess(
   io.dx  := Cat(~io.x(manW-adrW-1), io.x(manW-adrW-2,0))
 }
 
+// -------------------------------------------------------------------------
+//  _        _     _                        __  __
+// | |_ __ _| |__ | | ___    ___ ___   ___ / _|/ _|
+// | __/ _` | '_ \| |/ _ \  / __/ _ \ / _ \ |_| |_
+// | || (_| | |_) | |  __/ | (_| (_) |  __/  _|  _|
+//  \__\__,_|_.__/|_|\___|  \___\___/ \___|_| |_|
+// -------------------------------------------------------------------------
+
 class SqrtTableCoeff(
 
   val spec : RealSpec,
@@ -58,8 +74,8 @@ class SqrtTableCoeff(
   val calcW = manW + extraBits
 
   val io = IO(new Bundle {
-    val adr = Input (UInt((1+adrW).W))
-    val cs  = Flipped(new TableCoeffIO(maxCbit))
+    val adr = Input  (UInt((1+adrW).W))
+    val cs  = Flipped(new TableCoeffInput(maxCbit))
   })
 
   if(order == 0) {
@@ -101,6 +117,23 @@ class SqrtTableCoeff(
   }
 }
 
+// -------------------------------------------------------------------------
+//                        _        _     _                    _   _
+//  _ __   ___  _ __     | |_ __ _| |__ | | ___   _ __   __ _| |_| |__
+// | '_ \ / _ \| '_ \ ___| __/ _` | '_ \| |/ _ \ | '_ \ / _` | __| '_ \
+// | | | | (_) | | | |___| || (_| | |_) | |  __/ | |_) | (_| | |_| | | |
+// |_| |_|\___/|_| |_|    \__\__,_|_.__/|_|\___| | .__/ \__,_|\__|_| |_|
+//                                               |_|
+// -------------------------------------------------------------------------
+
+class SqrtNonTableOutput(val spec: RealSpec) extends Bundle {
+  val zex   = Output(UInt(spec.exW.W))
+  val zsgn  = Output(UInt(1.W))
+  val znan  = Output(Bool())
+  val zinf  = Output(Bool())
+  val zzero = Output(Bool())
+}
+
 // No pathway other than table interpolation. just calculate ex and sgn.
 class SqrtOtherPath(
   val spec : RealSpec, // Input / Output floating spec
@@ -115,12 +148,8 @@ class SqrtOtherPath(
   val exBias = spec.exBias
 
   val io = IO(new Bundle {
-    val x     = Input (UInt(spec.W.W))
-    val zex   = Output(UInt(spec.exW.W))
-    val zsgn  = Output(UInt(1.W))
-    val znan  = Output(Bool())
-    val zinf  = Output(Bool())
-    val zzero = Output(Bool())
+    val x      = Input(UInt(spec.W.W))
+    val zother = new SqrtNonTableOutput(spec)
   })
 
   val (xsgn,  xex,  xman) = FloatChiselUtil.decompose(spec, io.x)
@@ -132,17 +161,27 @@ class SqrtOtherPath(
   val zinf  = xinf
   val zzero = xzero || xneg
 
-  io.znan  := znan
-  io.zinf  := zinf
-  io.zzero := zzero
+  io.zother.znan  := znan
+  io.zother.zinf  := zinf
+  io.zother.zzero := zzero
 
   val xExNobias = xex - exBias.U
   val zex0 = xExNobias(exW-1) ## xExNobias(exW-1, 1) // arith right shift
-  io.zex  := Mux(zinf || znan, maskU(exW),
-             Mux(zzero,        0.U(exW.W),
-                               zex0 + exBias.U))
-  io.zsgn := 0.U(1.W) // always positive.
+
+  io.zother.zex  := Mux(zinf || znan, maskU(exW),
+                    Mux(zzero,        0.U(exW.W),
+                                      zex0 + exBias.U))
+  io.zother.zsgn := 0.U(1.W) // always positive.
 }
+
+// -------------------------------------------------------------------------
+//                  _
+//  _ __   ___  ___| |_ _ __  _ __ ___   ___ ___  ___ ___
+// | '_ \ / _ \/ __| __| '_ \| '__/ _ \ / __/ _ \/ __/ __|
+// | |_) | (_) \__ \ |_| |_) | | | (_) | (_|  __/\__ \__ \
+// | .__/ \___/|___/\__| .__/|_|  \___/ \___\___||___/___/
+// |_|                 |_|
+// -------------------------------------------------------------------------
 
 class SqrtPostProcess(
   val spec : RealSpec, // Input / Output floating spec
@@ -160,25 +199,28 @@ class SqrtPostProcess(
   def getStage() = nStage
 
   val io = IO(new Bundle {
-    // ex and some conditions
-    val zex   = Input(UInt(spec.exW.W))
-    val zsgn  = Input(UInt(1.W))
-    val znan  = Input(Bool())
-    val zinf  = Input(Bool())
-    val zzero = Input(Bool())
+    // ex and some flags
+    val zother = Flipped(new SqrtNonTableOutput(spec))
     // table interpolation results
-    val zres  = Input(UInt((spec.manW + extraBits).W))
+    val zres   = Input(UInt((spec.manW + extraBits).W))
     // output
-    val z     = Output(UInt(spec.W.W))
+    val z      = Output(UInt(spec.W.W))
   })
+
+  val zinf  = io.zother.zinf
+  val znan  = io.zother.znan
+  val zzero = io.zother.zzero
+
+  val zsgn = io.zother.zsgn
+  val zex  = io.zother.zex
 
   val zman0 = dropLSB(extraBits, io.zres) +& io.zres(extraBits-1)
   val polynomialOvf = zman0(manW)
-  val zeroFlush     = io.zinf || io.zzero || io.znan
-  val zman          = Mux(zeroFlush,     Cat(io.znan, 0.U((manW-1).W)),
+  val zeroFlush     = zinf || zzero || znan
+  val zman          = Mux(zeroFlush,     Cat(znan, 0.U((manW-1).W)),
                       Mux(polynomialOvf, Fill(manW,1.U(1.W)), zman0(manW-1,0)))
 
-  val z0 = Cat(io.zsgn, io.zex, zman)
+  val z0 = Cat(zsgn, zex, zman)
 
   io.z   := ShiftRegister(z0, nStage)
 }
