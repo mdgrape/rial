58e70af46aa67e6da396d899d56cc35c5a8af509
ToruNiina <niina.toru.68u@gmail.com>
2022-01-07 19:37:22 +0900

    Add test cases and disableTimeout flag to Pow2Test
    Since chiseltest considers a circuit that always output inf as idling,
    we need to disable timeout to run test correctly.

--- src/test/scala/mathfunc/Pow2Test.scala
+++ src/test/scala/mathfunc/Pow2Test.scala
@@ -61,13 +61,32 @@ class MathFuncPow2Test extends FlatSpec
   private def runtest ( spec : RealSpec,
       nOrder : Int, adrW : Int, extraBits : Int, stage : PipelineStageConfig,
       n : Int, r : Random, generatorStr : String,
-      generator : ( (RealSpec, Random) => RealGeneric)
+      generator : ( (RealSpec, Random) => RealGeneric),
+      disableTimeout : Boolean = false
   ) = {
     val total = stage.total
     val pipeconfig = stage.getString
     it should f"pow2(x) pipereg $pipeconfig spec ${spec.toStringShort} $generatorStr " in {
       test( new MathFunctions(spec, nOrder, adrW, extraBits, stage, false, false)) { c =>
         {
+          // To avoid timeoutException while testing z == inf/zero.
+          // Detailed explanation follows.
+          if(disableTimeout) { c.clock.setTimeout(0) }
+          // Detailed explanation:
+          //     Here, we disable timeout checking in chiseltest. By default,
+          // chiseltest fails after 1000 idle cycles. This is a reasonable
+          // behavior in most cases. However, unfortunately, the definition of
+          // "an idle cycle" is like: a cycle where input or output does not
+          // change. Since we are testing pow2 function, if we take a value
+          // larger than 128 or something, the result becomes inf. So, the test
+          // case [127, inf] fails after 1000 cycles. The behavior, returning
+          // inf, is the correct, expected behavior. But it is indistinguishable
+          // from doing nothing without any calculation. To avoid this false-
+          // positive, we manually disable timeout setting if we test large and
+          // small values that always return inf and zero, respectively. Note
+          // that, assertion and `io.expect` still works so there is no problem
+          // in the testing functionality.
+
           val maxCbit    = c.getMaxCbit
           val maxCalcW   = c.getMaxCalcW
           val nstage     = c.getStage
@@ -84,6 +103,7 @@ class MathFuncPow2Test extends FlatSpec
             c.io.y.poke(0.U(spec.W.W))
             val zi = c.io.z.peek.litValue.toBigInt
             if (i > nstage) {
+
               val (xid,z0d) = q.dequeue
 
               val xidsgn = bit(spec.W-1, xid).toInt
@@ -98,12 +118,7 @@ class MathFuncPow2Test extends FlatSpec
               val z0dexp = slice(spec.manW, spec.exW, z0d)
               val z0dman = z0d & maskSL(spec.manW)
 
-              if (zi != z0d) {
-                c.io.x.poke(xid.U(64.W))
-                for(i <- 1 to nstage) c.clock.step(1)
-                c.clock.step(1)
-              }
-
+              c.io.z.expect(z0d.U(spec.W.W))
               assert(zi == z0d, f"x = (${xidsgn}|${xidexp}(${xidexp - spec.exBias})|${xidman.toLong.toBinaryString}), test(${zisgn}|${ziexp}(${ziexp - spec.exBias})|${ziman.toLong.toBinaryString}) != ref(${z0dsgn}|${z0dexp}(${z0dexp - spec.exBias})|${z0dman.toLong.toBinaryString})")
             }
             c.clock.step(1)
@@ -113,13 +128,25 @@ class MathFuncPow2Test extends FlatSpec
     }
   }
 
-  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(),
-    n, r, "Test Safe Positive", generateRealWithin(0.0,  127.0, _,_) )
-  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(),
-    n, r, "Test Safe Negative", generateRealWithin(0.0, -126.0, _,_) )
-//   runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(),
-//     n, r, "Test Within (-128,128)",generateRealWithin(128.0,_,_))
-//   runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(),
-//     n, r, "Test All range",generateRealFull(_,_) )
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Safe Positive [1, 127]", generateRealWithin(1.0, 127.0,_,_))
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Safe Negative [-126, -1]", generateRealWithin(-126.0, -1.0,_,_))
+
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Large Positive [127, inf]", generateRealWithin(127.0, Double.PositiveInfinity,_,_), /*disableTimeout = */ true)
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Large Negative [-inf, -126]", generateRealWithin(Double.NegativeInfinity, -126.0,_,_), true)
+
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Small Positive [2^-7, 1]", generateRealWithin(pow(2.0, -7),1.0,_,_))
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Small Negative [-1, -2^-7]", generateRealWithin(-1.0, -pow(2.0, -7),_,_))
+
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Tiny Positive [0, 2^-7]", generateRealWithin(0.0, pow(2.0, -7),_,_))
+  runtest(RealSpec.Float32Spec, 2, 8, 2, PipelineStageConfig.none(), n, r,
+    "Test Tiny Negative [-2^-7, 0]", generateRealWithin(-pow(2.0, -7), 0.0,_,_))
+
 }
 
