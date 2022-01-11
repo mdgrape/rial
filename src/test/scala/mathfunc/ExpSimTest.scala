790e0cc7fd4915a9948057864bb8943c54a79e4c
ToruNiina <niina.toru.68u@gmail.com>
2022-01-11 18:12:58 +0900

    Add ExpSimTest

---
+++ src/test/scala/mathfunc/ExpSimTest.scala
@@ -0,0 +1,162 @@
+//package rial.tests
+
+//import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.FunSuite
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+
+//import org.scalatest.flatspec.AnyFlatSpec
+//import org.scalatest.matchers.should.Matchers
+//import scopt.OptionParser
+
+import scala.util.Random
+import scala.math._
+
+import spire.math.SafeLong
+import spire.math.Numeric
+import spire.implicits._
+
+import rial.math.ExponentialSim
+import rial.mathfunc._
+import rial.util.ScalaUtil._
+import rial.testUtil.ScalaTestUtil._ // errorLSB
+import rial.arith._
+import rial.table._
+
+class MathFuncExpSimTest extends FunSuite with BeforeAndAfterAllConfigMap {
+  var n = 1000000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("10000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRealWithin( from : Double, to : Double, spec: RealSpec, r : Random ) = {
+    val rD : Double = from + r.nextDouble() * (to - from)
+    new RealGeneric(spec, rD)
+  }
+
+  def expTest(t : FuncTableInt, spec : RealSpec, n : Int, r : Random,
+    generatorStr    : String,
+    generator       : ( (RealSpec, Random) => RealGeneric),
+    tolerance       : Int ) = {
+    test(s"exp(x), format ${spec.toStringShort}, ${generatorStr}") {
+
+//       val libc = Native.loadLibrary("c", classOf[libc]).asInstanceOf[libc]
+
+      var maxError    = SafeLong(0)
+      var xatMaxError = 0.0
+      var zatMaxError = 0.0
+
+      var err1lsbPos = 0
+      var err1lsbNeg = 0
+      var err2lsbPos = 0
+      var err2lsbNeg = 0
+      var errNlsbPos = 0
+      var errNlsbNeg = 0
+      for(i <- 1 to n) {
+        val x  = generator(spec,r)
+        val x0 = x.toDouble
+
+        val z0   = exp(x0) // .toFloat
+        val z0r  = new RealGeneric(spec, z0)
+
+        val zi   = MathFuncExpSim.expSimGeneric( t, x )
+        val zd   = zi.toDouble
+        val erri = errorLSB(zi, z0r)
+
+        if (x0.isInfinity) {
+          if(0 < x0) {
+            assert(zi.isInfinite)
+          } else {
+            assert(zi.isZero)
+          }
+        } else if (x0.isNaN) {
+          assert(zi.isNaN)
+        } else {
+          if (erri.abs >= 2) {
+            val xsgn = bit(spec.W-1, x.value).toInt
+            val xexp = slice(spec.manW, spec.exW, x.value)
+            val xman = x.value & maskSL(spec.manW)
+
+            val zsimsgn = bit(spec.W-1, zi.value).toInt
+            val zsimexp = slice(spec.manW, spec.exW, zi.value)
+            val zsimman = zi.value & maskSL(spec.manW)
+
+            val zrefsgn = bit(spec.W-1, z0r.value).toInt
+            val zrefexp = slice(spec.manW, spec.exW, z0r.value)
+            val zrefman = z0r.value & maskSL(spec.manW)
+
+            if(erri.abs > tolerance) {
+              println(f"z0  = ${z0}")
+              println(f"z0r = ${z0r.sgn}| ${z0r.ex}| ${z0r.man.toLong.toBinaryString}")
+              println(f"zi  = ${zi.sgn }| ${zi .ex}| ${zi .man.toLong.toBinaryString}")
+              println(f"zd  = ${zd}")
+              println(f"test: x   = ${x0}(${x.sgn}|${x.ex}(${x.ex-x.spec.exBias})|${x.man.toLong.toBinaryString})")
+              println(f"test: ref = ${z0}(${zrefsgn}|${zrefexp}(${zrefexp-x.spec.exBias})|${zrefman.toLong.toBinaryString})")
+              println(f"test: sim = ${zd}(${zsimsgn}|${zsimexp}(${zsimexp-x.spec.exBias})|${zsimman.toLong.toBinaryString})")
+              println(f"test: test(${zsimsgn}|${zsimexp}(${zsimexp - spec.exBias})|${zsimman.toLong.toBinaryString}(${zsimman.toLong}%x)) != ref(${zrefsgn}|${zrefexp}(${zrefexp - spec.exBias})|${zrefman.toLong.toBinaryString}(${zrefman.toLong}%x))")
+            }
+
+            if(erri > 2) {
+              errNlsbPos += 1
+            } else if(erri < -2) {
+              errNlsbNeg += 1
+            } else if(erri > 1) {
+              err2lsbPos += 1
+            } else {
+              err2lsbNeg += 1
+            }
+
+          } else if (erri>=1) {
+            err1lsbPos+=1
+          }
+          else if (erri<= -1) {
+            err1lsbNeg+=1
+          }
+          assert(erri.abs<=tolerance)
+
+          if (maxError < erri.abs) {
+            maxError    = erri.abs
+            xatMaxError = x0
+            zatMaxError = zd
+          }
+        }
+      }
+      println(f"${generatorStr} Summary")
+      if(maxError != 0.0) {
+        println(f"N=$n%d : largest errors ${maxError.toInt}%d where the value is "
+              + f"${zatMaxError} != ${exp(xatMaxError)}, "
+              + f"diff = ${zatMaxError - exp(xatMaxError)}, x = ${xatMaxError}")
+      }
+      println(f"N=$n%d : 1LSB errors positive $err1lsbPos%d / negative $err1lsbNeg%d")
+      println(f"N=$n%d : 2LSB errors positive $err2lsbPos%d / negative $err2lsbNeg%d")
+      println(f"N=$n%d : 2<   errors positive $errNlsbPos%d / negative $errNlsbNeg%d")
+      println( "---------------------------------------------------------------")
+    }
+  }
+
+  val pow2F32TableI = ExponentialSim.pow2TableGeneration(
+    2, 8, RealSpec.Float32Spec.manW, RealSpec.Float32Spec.manW+2)
+
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Safe Positive [1, 127]", generateRealWithin(1.0, 127.0,_,_), 2)
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Safe Negative [-126, -1]", generateRealWithin(-126.0, -1.0,_,_), 2)
+
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Large Positive [127, inf]", generateRealWithin(127.0, Double.PositiveInfinity,_,_), 2)
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Large Negative [-inf, -126]", generateRealWithin(Double.NegativeInfinity, -126.0,_,_), 2)
+
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Small Positive [2^-7, 1]", generateRealWithin(pow(2.0, -7),1.0,_,_), 2)
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Small Negative [-1, -2^-7]", generateRealWithin(-1.0, -pow(2.0, -7),_,_), 2)
+
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Tiny Positive [0, 2^-7]", generateRealWithin(0.0, pow(2.0, -7),_,_), 2)
+  expTest(pow2F32TableI, RealSpec.Float32Spec, n, r,
+    "Test Tiny Negative [-2^-7, 0]", generateRealWithin(-pow(2.0, -7), 0.0,_,_), 2)
+}
