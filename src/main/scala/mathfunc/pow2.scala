d4d51c7d1b209457c10e1f4f3e0e33009fb30b85
ToruNiina <niina.toru.68u@gmail.com>
2022-02-07 20:23:03 +0900

    Merge exp and pow2 by adding exp-path to pow2
    To reduce required area

--- src/main/scala/mathfunc/pow2.scala
+++ src/main/scala/mathfunc/pow2.scala
@@ -16,6 +16,7 @@ import rial.util.ScalaUtil._
 import rial.util.PipelineStageConfig._
 import rial.arith.RealSpec
 import rial.arith.RealGeneric
+import rial.arith.RoundSpec
 import rial.arith.FloatChiselUtil
 
 // -------------------------------------------------------------------------
@@ -48,6 +49,7 @@ class Pow2PreProcess(
   val padding   = extraBits
 
   val io = IO(new Bundle {
+    val isexp     = Input (Bool())
     val x         = Input (UInt(spec.W.W))
     val adr       = Output(UInt(adrW.W))
     val dx        = if(order != 0) { Some(Output(UInt(dxW.W))) } else { None }
@@ -58,13 +60,11 @@ class Pow2PreProcess(
     // LSB of fractional part of x, for zman correction
     val xfracLSBs = if(padding != 0) {Some(Output(UInt(padding.W)))} else {None}
 
-    // Used in ExpPreProcess, but not used here. Always zero.
+    // if pow2, always zero. if exp, 1 if (x * log2e).ex is larger than x.ex.
     val xexd = Output(UInt(1.W))
   })
 
-  io.xexd := 0.U(1.W)
-
-  val (xsgn, xex, xman) = FloatChiselUtil.decompose(spec, io.x)
+  val (xsgn, xex0, xman0) = FloatChiselUtil.decompose(spec, io.x)
 
   val log2 = (a:Double) => {log(a) / log(2.0)}
   val xExOvfLimit = math.ceil(log2(maskL(exW)-exBias)).toLong // log2(255-127 = 128) = 7
@@ -72,9 +72,59 @@ class Pow2PreProcess(
 
   val xIntW  = max(xExOvfLimit, xExUdfLimit).toInt
   val xFracW = manW + padding
+  val xValW  = xIntW + xFracW + 1
 
-  val xValW = xIntW + xFracW + 1
-  val xVal  = Cat(1.U(1.W), xman, 0.U((xIntW + padding).W))
+  val extraMan = padding + xIntW
+
+  // ------------------------------------------------------------------------
+  // for exponential, multiply x by log2e.
+
+  // XXX the width (37) is determined empirically
+  val log2eSpec = new RealSpec(8, 0x7F, 37, false, false, true)
+  val log2e = new RealGeneric(log2eSpec, log2(E)) // ~ 1.4427
+
+  val xprod = Cat(1.U(1.W), xman0) * log2e.manW1.toBigInt.U((log2eSpec.manW+1).W)
+  val xprodW = xprod.getWidth
+  assert(xprodW == (1+manW) + (1+log2eSpec.manW))
+  val xprodMoreThan2 = xprod(xprodW - 1) === 1.U
+
+  assert(log2eSpec.manW - extraMan > 0)
+
+  val xprodbp        = spec.manW + log2eSpec.manW
+  val xprodRoundBits = xprodbp - (spec.manW + extraMan)
+
+  val xprodSticky  = (xprodMoreThan2 & xprod(xprodRoundBits-1)) |
+                     xprod(xprodRoundBits-2, 0).orR
+  val xprodRound   = Mux(xprodMoreThan2, xprod(xprodRoundBits), xprod(xprodRoundBits-1))
+  val xprodShifted = Mux(xprodMoreThan2, xprod(xprodbp, xprodRoundBits+1),
+                                         xprod(xprodbp-1, xprodRoundBits))
+  val xprodLSB     = xprodShifted(0)
+
+  val xprodInc = FloatChiselUtil.roundIncBySpec(RoundSpec.roundToEven,
+    xprodLSB, xprodRound, xprodSticky)
+
+  val xprodRounded = xprodShifted +& xprodInc
+  assert(xprodRounded.getWidth == manW+1 + extraMan)
+  val xprodMoreThan2AfterRounded = xprodRounded(manW+extraMan)
+
+  val xmanExp = xprodRounded(manW+extraMan-1, 0)
+  val xexExp  = xex0 + xprodMoreThan2AfterRounded + xprodMoreThan2
+
+  assert(xprodMoreThan2AfterRounded +& xprodMoreThan2 =/= 2.U)
+
+  val xexd0 = io.isexp & (xprodMoreThan2AfterRounded + xprodMoreThan2)
+  io.xexd := ShiftRegister(xexd0, nStage)
+
+  // ------------------------------------------------------------------------
+  // select exp or pow2 based on io.isexp
+
+  val xex  = Mux(io.isexp, xexExp,  xex0)
+  val xman = Mux(io.isexp, xmanExp, Cat(xman0, 0.U(extraMan.W)))
+
+  val xVal = Cat(1.U(1.W), xman)
+
+  // ------------------------------------------------------------------------
+  // do preprocess
 
   val xshift0 = (xIntW + exBias).U(exW.W) - xex
   val xshift  = xshift0(xIntW-1, 0)
@@ -217,7 +267,7 @@ class Pow2OtherPath(
   val io = IO(new Bundle {
     val x         = Flipped(new DecomposedRealOutput(spec))
     val xint      = Input(UInt(exW.W))
-    val xexd      = Input(UInt(1.W)) // used in ExpPreProcess.
+    val xexd      = Input(UInt(1.W)) // if exp & (x * log2e).ex is larger than x.ex, 1
     // for z correction
     val xfracLSBs = if(padding != 0) {Some(Input (UInt(padding.W  )))} else {None}
     val zCorrCoef = if(padding != 0) {Some(Output(UInt(zCorrTmpW.W)))} else {None}
