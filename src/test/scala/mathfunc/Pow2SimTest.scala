5238d0686ba87295275abf1417bb9688660f10cc
ToruNiina <niina.toru.68u@gmail.com>
2022-03-17 14:22:31 +0900

    Fix paramters in Pow2SimTest

--- src/test/scala/mathfunc/Pow2SimTest.scala
+++ src/test/scala/mathfunc/Pow2SimTest.scala
@@ -176,31 +176,31 @@ class MathFuncPow2SimTest extends AnyFunSuite with BeforeAndAfterAllConfigMap {
   pow2Test(pow2FP32TableI, RealSpec.Float32Spec, n, r,
     "Test Tiny Negative [-2^-7, 0]", generateRealWithin(-pow(2.0, -7), 0.0,_,_), 2)
 
-  val nOrderBF16 = 2
-  val adrWBF16 = 8
-  val extraBitsBF16 = 3
+  val nOrderBF16 = 0
+  val adrWBF16 = 7
+  val extraBitsBF16 = 1
 
   val pow2BF16TableI = ExponentialSim.pow2TableGeneration(
     nOrderBF16, adrWBF16, RealSpec.BFloat16Spec.manW, RealSpec.BFloat16Spec.manW+extraBitsBF16)
 
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Safe Positive [1, 127]", generateRealWithin(1.0, 127.0,_,_), 2)
+    "Test Safe Positive [1, 127]", generateRealWithin(1.0, 127.0,_,_), 1)
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Safe Negative [-126, -1]", generateRealWithin(-126.0, -1.0,_,_), 2)
+    "Test Safe Negative [-126, -1]", generateRealWithin(-126.0, -1.0,_,_), 1)
 
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Large Positive [127, inf]", generateRealWithin(127.0, Double.PositiveInfinity,_,_), 2)
+    "Test Large Positive [127, inf]", generateRealWithin(127.0, Double.PositiveInfinity,_,_), 1)
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Large Negative [-inf, -126]", generateRealWithin(Double.NegativeInfinity, -126.0,_,_), 2)
+    "Test Large Negative [-inf, -126]", generateRealWithin(Double.NegativeInfinity, -126.0,_,_), 1)
 
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Small Positive [2^-7, 1]", generateRealWithin(pow(2.0, -7),1.0,_,_), 2)
+    "Test Small Positive [2^-7, 1]", generateRealWithin(pow(2.0, -7),1.0,_,_), 1)
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Small Negative [-1, -2^-7]", generateRealWithin(-1.0, -pow(2.0, -7),_,_), 2)
+    "Test Small Negative [-1, -2^-7]", generateRealWithin(-1.0, -pow(2.0, -7),_,_), 1)
 
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Tiny Positive [0, 2^-7]", generateRealWithin(0.0, pow(2.0, -7),_,_), 2)
+    "Test Tiny Positive [0, 2^-7]", generateRealWithin(0.0, pow(2.0, -7),_,_), 1)
   pow2Test(pow2BF16TableI, RealSpec.BFloat16Spec, n, r,
-    "Test Tiny Negative [-2^-7, 0]", generateRealWithin(-pow(2.0, -7), 0.0,_,_), 2)
+    "Test Tiny Negative [-2^-7, 0]", generateRealWithin(-pow(2.0, -7), 0.0,_,_), 1)
 
 }
