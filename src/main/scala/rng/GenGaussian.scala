e83cec33ba1e4addd32ee5b07d46cebf46840a7c
ToruNiina <niina.toru.68u@gmail.com>
2022-04-22 18:47:15 +0900

    Add fixedpoint sin/cos(2pi*x) for Box-Muller

---
+++ src/main/scala/rng/GenGaussian.scala
@@ -0,0 +1,120 @@
+//% @file GenGaussian.scala
+//
+// Copyright (C) Toru Niina RIKEN BDR 2022
+//
+package rial.rng
+
+import scala.language.reflectiveCalls
+import scala.math._
+
+import chisel3._
+import chisel3.util._
+
+import rial.arith._
+import rial.math._
+import rial.table._
+import rial.util._
+import rial.util.RialChiselUtil._
+
+//
+// sin/cos are used to determine the argument, so y can be a fixed point.
+// By doing this, the resolution (the difference between the current and the
+// next value) around 2piy ~ 0, pi, 2pi will be almost the same. It is not a
+// problem.
+//
+class SinCos2Pi(
+  rndW: Int,      // width of input fixedpoint
+  spec: RealSpec, // output width
+  polySpec: PolynomialSpec,
+  stage: PipelineStageConfig
+  ) extends Module {
+
+  val nStage = stage.total
+
+  val exW    = spec.exW
+  val manW   = spec.manW
+  val exBias = spec.exBias
+
+  val order  = polySpec.order
+  val fracW  = polySpec.fracW
+  val adrW   = polySpec.adrW
+  val dxW    = polySpec.dxW
+
+  assert(rndW > 2 + adrW + dxW) // manW + 2; satisfied by (i32, f32) or (i64, f64)
+
+  val io = IO(new Bundle {
+    val isSin = Input(Bool())
+    val x     = Input(UInt(rndW.W))
+    val z     = Output(UInt(spec.W.W))
+  })
+
+  // convert x in [0, 1) to [0, 1/4).
+  val zsgn = Wire(UInt(1.W))
+  val x = Wire(UInt((rndW-2).W))
+  when(io.isSin) {
+    zsgn := io.x(rndW-1)
+    x := Mux(io.x(rndW-2) === 1.U, ~(io.x(rndW-3, 0))+1.U, io.x(rndW-3, 0))
+  } otherwise {
+    zsgn := io.x(rndW-1) ^ io.x(rndW-2)
+    x := Mux(io.x(rndW-2) === 0.U, ~(io.x(rndW-3, 0))+1.U, io.x(rndW-3, 0))
+  }
+
+  val adr = x(x.getWidth-1, x.getWidth-adrW)
+
+  // in a range x in [0, 1/4), 4 < sin(2pix)/x <= 2pi.
+  // So 1/2 < sin(2pix)/8x <= pi/4 = 0.78.. < 1.
+  val tableD = new FuncTableDouble( x0 => {
+    val x = x0 / 4.0 // convert [0, 1) to the input range, [0, 1/4)
+    sin(x * Pi * 2.0) / (8.0*x)
+  }, order )
+  tableD.addRange(0.0, 1.0, 1<<adrW)
+  val tableI = new FuncTableInt( tableD, fracW )
+  val cbit = tableI.cbit
+
+  val (coeffTable, coeffWidth) = tableI.getVectorUnified(/*sign mode =*/0)
+  val coeff = getSlices(coeffTable(adr), coeffWidth)
+
+  val polyEval = Module(new PolynomialEval(spec, polySpec, cbit, stage))
+
+  val coeffs = Wire(new TableCoeffInput(cbit))
+  for (i <- 0 to order) {
+    coeffs.cs(i) := coeff(i)
+  }
+
+  polyEval.io.coeffs := coeffs
+  if(order != 0) {
+    val dx = Cat(~x(x.getWidth-adrW-1), x(x.getWidth-adrW-2, x.getWidth-adrW-dxW))
+    polyEval.io.dx.get := dx
+  }
+
+  val polyRes = polyEval.io.result // sin(2pix)/8x, in [0.5, 1). W = fracW
+
+  // multiply x * sin(2pix)/x
+
+  val xzero  = x === 0.U
+  val xShift = Mux(xzero, 0.U, PriorityEncoder(Reverse(x)))
+  val xAligned = (x << xShift)(x.getWidth-1, 0)
+  assert(xAligned(x.getWidth-1) === 1.U)
+
+  val zProd      = xAligned * polyRes
+  val zProdW     = x.getWidth + fracW
+
+  val zMoreThan2 = zProd(fracW+x.getWidth - 1)
+  val zShifted   = Mux(zMoreThan2, zProd(zProdW-2, zProdW-manW-1),
+                                   zProd(zProdW-3, zProdW-manW-2))
+
+  val zRounded   = zShifted +& Mux(zMoreThan2, zProd(zProdW-manW-2),
+                                               zProd(zProdW-manW-3))
+  val zMoreThan2AfterRound = zRounded(manW)
+  val zmanW1     = Mux(zMoreThan2AfterRound, Cat(1.U(1.W), 0.U(manW.W)),
+                                             Cat(1.U(1.W), zRounded(manW-1, 0)))
+  val zexInc     = zMoreThan2 + zMoreThan2AfterRound
+
+  val zex  = Mux(xzero, 0.U, (exBias-1).U(exW.W) - xShift + zexInc)
+  val zman = Mux(xzero, 0.U, zmanW1(manW-1, 0))
+
+  val z = Cat(zsgn, zex, zman)
+  assert(z.getWidth == spec.W)
+
+  io.z := z
+}
---
+++ src/test/scala/rng/GenGaussianTest.scala
@@ -0,0 +1,118 @@
+import org.scalatest._
+
+import chisel3._
+import chiseltest._
+import chiseltest.VerilatorBackendAnnotation
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.{BeforeAndAfterAllConfigMap, ConfigMap}
+
+import spire.math.SafeLong
+import spire.math.Numeric
+import spire.implicits._
+
+import rial.arith._
+import rial.math._
+import rial.rng._
+import rial.util.ScalaUtil._
+import rial.util.PipelineStageConfig
+
+import scala.util.Random
+import scala.math._
+import scala.collection.mutable.Queue
+import scala.language.reflectiveCalls
+
+//
+// Test GenRandomFloat12 using chi-squared test.
+//
+
+class SinCos2PiTest extends AnyFlatSpec
+    with ChiselScalatestTester with Matchers with BeforeAndAfterAllConfigMap {
+
+  behavior of "Test uniform real distribution [1, 2)"
+
+  var n = 1000
+
+  override def beforeAll(configMap: ConfigMap) = {
+    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    println(s"ncycle=$n")
+  }
+
+  val r = new Random(123456789)
+
+  def generateRandomUInt(width: Int, r: Random) = {
+    val rnd = SafeLong(r.nextLong)
+    rnd & maskSL(width)
+  }
+
+  private def runtest ( isSin: Boolean, rndW: Int, spec : RealSpec, polySpec: PolynomialSpec,
+      stage: PipelineStageConfig,
+      n : Int, r : Random, generatorStr : String,
+      generator : ( (Int, Random) => SafeLong),
+      tolerance : Int
+  ) = {
+    val total = stage.total
+    val pipeconfig = stage.getString
+    it should f"sin/cos(2pi*x) pipereg $pipeconfig spec ${spec.toStringShort} $generatorStr " in {
+      test( new SinCos2Pi(rndW, spec, polySpec, stage)).
+        withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+        {
+          val nstage = stage.total
+
+          val reference = (x: SafeLong) => {
+            val xr = x.toDouble / (SafeLong(1) << rndW).toDouble
+            val z  = if(isSin) {sin(2*Pi*xr)} else {cos(2*Pi*xr)}
+            new RealGeneric(spec, z)
+          }
+
+          val q  = new Queue[(BigInt,BigInt)]
+          for(i <- 1 to n+nstage) {
+            val xi = generator(rndW, r)
+            val z0r= reference(xi)
+            q += ((xi.toBigInt, z0r.value.toBigInt))
+
+            c.io.isSin.poke(isSin.B)
+            c.io.x.poke(xi.toBigInt.U(spec.W.W))
+            val zi = c.io.z.peek().litValue.toBigInt
+            c.clock.step(1)
+            if (i > nstage) {
+              val (xid,z0d) = q.dequeue()
+
+              val xidsgn = bit(spec.W-1, xid).toInt
+              val xidexp = slice(spec.manW, spec.exW, xid)
+              val xidman = xid & maskSL(spec.manW)
+
+              val zisgn = bit(spec.W-1, zi).toInt
+              val ziexp = slice(spec.manW, spec.exW, zi)
+              val ziman = zi & maskSL(spec.manW)
+
+              val z0dsgn = bit(spec.W-1, z0d).toInt
+              val z0dexp = slice(spec.manW, spec.exW, z0d)
+              val z0dman = z0d & maskSL(spec.manW)
+
+              val diff = (zi - z0d).abs
+
+              val xr = xid.toDouble / (SafeLong(1) << rndW).toDouble
+              val xc = if(xr < 0.25) {xr} else if(xr < 0.5) {0.5 - xr} else if(xr < 0.75) {xr - 0.5} else {1.0 - xr}
+
+              assert(diff < tolerance,
+                     f"x = ${xid.toLong.toBinaryString}(${xr} -> ${xc}), z = ${sin(2 * Pi * xr)}, "+
+                     f"test(${zisgn}|${ziexp}(${ziexp - spec.exBias})|${ziman.toLong.toBinaryString})(${new RealGeneric(spec, zisgn, ziexp.toInt, ziman).toDouble}) != " +
+                     f"ref(${z0dsgn}|${z0dexp}(${z0dexp - spec.exBias})|${z0dman.toLong.toBinaryString})(${new RealGeneric(spec, z0dsgn, z0dexp.toInt, z0dman).toDouble})")
+            }
+          }
+        }
+      }
+    }
+  }
+  val nOrderFP32    = 2
+  val adrWFP32      = 8
+  val extraBitsFP32 = 3
+  val polySpecFP32  = new PolynomialSpec(RealSpec.Float32Spec, nOrderFP32, adrWFP32, extraBitsFP32)
+
+  runtest(true,  32, RealSpec.Float32Spec, polySpecFP32, PipelineStageConfig.none,
+    n, r, "Test FP32 sin",generateRandomUInt, 3)
+  runtest(false, 32, RealSpec.Float32Spec, polySpecFP32, PipelineStageConfig.none,
+    n, r, "Test FP32 cos",generateRandomUInt, 3)
+}
