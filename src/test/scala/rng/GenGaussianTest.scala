db562bf6f8f93687832870c28e3e1b79b042f6f3
ToruNiina <niina.toru.68u@gmail.com>
2022-04-26 15:51:07 +0900

    Test SinCos2Pi in Box Muller with special values

--- src/test/scala/rng/GenGaussianTest.scala
+++ src/test/scala/rng/GenGaussianTest.scala
@@ -32,10 +32,10 @@ class SinCos2PiTest extends AnyFlatSpec
 
   behavior of "Test uniform real distribution [1, 2)"
 
-  var n = 1000
+  var n = 10000
 
   override def beforeAll(configMap: ConfigMap) = {
-    n = configMap.getOptional[String]("n").getOrElse("1000").toInt
+    n = configMap.getOptional[String]("n").getOrElse("10000").toInt
     println(s"ncycle=$n")
   }
 
@@ -62,7 +62,11 @@ class SinCos2PiTest extends AnyFlatSpec
 
           val reference = (x: SafeLong) => {
             val xr = x.toDouble / (SafeLong(1) << rndW).toDouble
-            val z  = if(isSin) {sin(2*Pi*xr)} else {cos(2*Pi*xr)}
+            val z  = if(isSin) {
+              if(x == (SafeLong(0) << (rndW-2)) || x == (SafeLong(2) << (rndW-2))) {0.0} else {sin(2 * Pi * xr)}
+            } else {
+              if(x == (SafeLong(1) << (rndW-2)) || x == (SafeLong(3) << (rndW-2))) {0.0} else {cos(2 * Pi * xr)}
+            }
             new RealGeneric(spec, z)
           }
 
@@ -96,11 +100,51 @@ class SinCos2PiTest extends AnyFlatSpec
               val xr = xid.toDouble / (SafeLong(1) << rndW).toDouble
               val xc = if(xr < 0.25) {xr} else if(xr < 0.5) {0.5 - xr} else if(xr < 0.75) {xr - 0.5} else {1.0 - xr}
 
-              assert(diff < tolerance,
+              assert(diff <= tolerance,
                      f"x = ${xid.toLong.toBinaryString}(${xr} -> ${xc}), z = ${sin(2 * Pi * xr)}, "+
                      f"test(${zisgn}|${ziexp}(${ziexp - spec.exBias})|${ziman.toLong.toBinaryString})(${new RealGeneric(spec, zisgn, ziexp.toInt, ziman).toDouble}) != " +
                      f"ref(${z0dsgn}|${z0dexp}(${z0dexp - spec.exBias})|${z0dman.toLong.toBinaryString})(${new RealGeneric(spec, z0dsgn, z0dexp.toInt, z0dman).toDouble})")
             }
+          }
+
+          // special values: 0, 0.25, 0.5, 0.75, 1-eps
+          for(xid <- Seq(SafeLong(0), SafeLong(1)<<(rndW-2), SafeLong(2)<<(rndW-2), SafeLong(3)<<(rndW-2), maskSL(rndW) )) {
+            val z0r = reference(xid)
+            val z0d = z0r.value.toBigInt
+
+            c.io.isSin.poke(isSin.B)
+            c.io.x.poke(xid.toBigInt.U(spec.W.W))
+            for(j <- 0 until nstage) {
+              c.clock.step(1)
+            }
+            val zi = c.io.z.peek().litValue.toBigInt
+
+            val diff = (zi - z0d).abs
+
+            val xr = xid.toDouble / (SafeLong(1) << rndW).toDouble
+            val xc = if(xr < 0.25) {xr} else if(xr < 0.5) {0.5 - xr} else if(xr < 0.75) {xr - 0.5} else {1.0 - xr}
+            val zr = if(isSin) {sin(2 * Pi * xr)} else {cos(2 * Pi * xr)}
+
+            val xidsgn = bit(spec.W-1, xid).toInt
+            val xidexp = slice(spec.manW, spec.exW, xid)
+            val xidman = xid & maskSL(spec.manW)
+
+            val zisgn = bit(spec.W-1, zi).toInt
+            val ziexp = slice(spec.manW, spec.exW, zi)
+            val ziman = zi & maskSL(spec.manW)
+
+            val z0dsgn = bit(spec.W-1, z0d).toInt
+            val z0dexp = slice(spec.manW, spec.exW, z0d)
+            val z0dman = z0d & maskSL(spec.manW)
+
+            if(diff > tolerance) {
+              c.clock.step(1) // run printf
+            }
+
+            assert(diff <= tolerance,
+                   f"x = ${xid.toLong.toBinaryString}(${xr} -> ${xc}), z(scala.math.FP64) = ${zr}, "+
+                   f"test(${zisgn}|${ziexp}(${ziexp - spec.exBias})|${ziman.toLong.toBinaryString})(${new RealGeneric(spec, zisgn, ziexp.toInt, ziman).toDouble}) != " +
+                   f"ref(${z0dsgn}|${z0dexp}(${z0dexp - spec.exBias})|${z0dman.toLong.toBinaryString})(${new RealGeneric(spec, z0dsgn, z0dexp.toInt, z0dman).toDouble})")
           }
         }
       }
