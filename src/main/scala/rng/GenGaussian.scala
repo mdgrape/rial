d9cbe2743ae8e5e74831b13b7bc72130571765c3
ToruNiina <niina.toru.68u@gmail.com>
2022-04-26 17:44:47 +0900

    Add sqrt(-2log(x)) to Box-Muller

--- src/main/scala/rng/GenGaussian.scala
+++ src/main/scala/rng/GenGaussian.scala
@@ -15,6 +15,14 @@ import rial.math._
 import rial.table._
 import rial.util._
 import rial.util.RialChiselUtil._
+import rial.util.ScalaUtil._
+
+//
+// generate Gaussian using Box-Muller.
+//
+// z1 = sqrt(-2log(x)) cos(2pi*y)
+// z2 = sqrt(-2log(x)) sin(2pi*y)
+//
 
 //
 // sin/cos are used to determine the argument, so y can be a fixed point.
@@ -134,6 +142,99 @@ class SinCos2Pi(
 
   val zex  = Mux(zone, exBias.U(exW.W), Mux(xzero, 0.U, (exBias-1).U(exW.W) - xShift + zexInc))
   val zman = Mux(zone || xzero, 0.U, zmanW1(manW-1, 0))
+
+  val z = Cat(zsgn, zex, zman)
+  assert(z.getWidth == spec.W)
+
+  io.z := z
+}
+
+//
+// z = sqrt(-2log(1-x)) at fixedpoint precision.
+//
+class Sqrt2LogX(
+  rndW: Int,      // width of input fixedpoint
+  spec: RealSpec, // output width
+  polySpec: PolynomialSpec,
+  stage: PipelineStageConfig
+  ) extends Module {
+
+  val nStage = stage.total
+
+  val exW    = spec.exW
+  val manW   = spec.manW
+  val exBias = spec.exBias
+
+  val order  = polySpec.order
+  val fracW  = polySpec.fracW
+  val extraBits = fracW - manW
+  val adrW   = polySpec.adrW
+  val dxW    = polySpec.dxW
+
+  assert(rndW >= adrW + dxW)
+
+  val io = IO(new Bundle {
+    val x = Input(UInt(rndW.W))
+    val z = Output(UInt(spec.W.W))
+  })
+
+  val x = io.x
+
+  // if rndW=32, xmin = 2^-32. then sqrt(-2 * log(2^-32)) ~ 6.66. intW = 3.
+  val xmin = pow(2.0, -rndW)
+  val zmax = sqrt(-2.0 * log(xmin))
+  val intW = log2UpD(zmax)
+  if(extraBits < intW) {
+    println(f"WARNING: extraBits(${extraBits}) in polynomial is less than "+
+            f"the recommended intW(${intW}).")
+  }
+
+  // sqrt(-2log(1-x)); to avoid inf, the x axis is reversed
+  val tableD = new FuncTableDouble( x0 => {
+    val scale = pow(2.0, -intW) // scale to make z < 1
+    if(x0 < xmin) {
+      zmax * scale
+    } else {
+      sqrt(-2*log(1.0-x0)) * scale
+    }
+  }, order )
+  tableD.addRange(0.0, 1.0, 1<<adrW)
+  val tableI = new FuncTableInt( tableD, fracW )
+  val cbit = tableI.cbit
+  val (coeffTable, coeffWidth) = tableI.getVectorUnified(/*sign mode =*/0)
+
+  val polyEval = Module(new PolynomialEval(spec, polySpec, cbit, stage))
+
+  val adr   = x(rndW-1, rndW-adrW)
+  val coeff = getSlices(coeffTable(adr), coeffWidth)
+
+  val coeffs = Wire(new TableCoeffInput(cbit))
+  for (i <- 0 to order) {
+    coeffs.cs(i) := coeff(i)
+  }
+
+  polyEval.io.coeffs := coeffs
+  if(order != 0) {
+    val dx = Cat(~x(x.getWidth-adrW-1), x(x.getWidth-adrW-2, x.getWidth-adrW-dxW))
+    polyEval.io.dx.get := dx
+  }
+
+  val zres = polyEval.io.result // sin(2pix)/8x, in [0.5, 1). W = fracW
+
+  // shift to normalize z
+
+  val zShift = Mux(zres === 0.U, 0.U, PriorityEncoder(Reverse(zres)))
+  val zAligned = (zres << zShift)(fracW-1, 0)
+  assert(zAligned(fracW-1) === 1.U || zres === 0.U)
+
+  assert(0 <= fracW-manW-2)
+  val zman0 = zAligned(fracW-2, fracW-manW-1) +& zAligned(fracW-manW-2)
+  val zmanMoreThan2AfterRound = zman0(manW)
+  val zex0 = (exBias + intW - 1).U(exW.W) - zShift + zmanMoreThan2AfterRound
+
+  val zman = zman0(manW-1, 0)
+  val zex  = Mux(zres === 0.U, 0.U, zex0)
+  val zsgn = 0.U(1.W)
 
   val z = Cat(zsgn, zex, zman)
   assert(z.getWidth == spec.W)
