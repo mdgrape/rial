f2cdf1312e6c7e0c73066e7d2a45107c6ea9d62f
ToruNiina <niina.toru.68u@gmail.com>
2022-05-18 18:54:25 +0900

    Simplify atan2 stage2; decrease num of tables

--- src/main/scala/math/atan2Stage2Sim.scala
+++ src/main/scala/math/atan2Stage2Sim.scala
@@ -27,9 +27,12 @@ import rial.arith._
 
 object ATan2Stage2Sim {
 
+  // Since atan2-stage1 calculates min(x,y)/max(x,y), so it assumes x <= 1.
   def atan2Stage2SimGeneric(
-    ts : Seq[FuncTableInt], x : RealGeneric, status: Int, special: Int, ysgn: Int
+    t : FuncTableInt, x : RealGeneric, status: Int, special: Int, ysgn: Int
     ): RealGeneric = {
+
+    assert(x.toDouble <= 1.0)
 
 //     println("==================================================")
 
@@ -37,9 +40,9 @@ object ATan2Stage2Sim {
     val manW   = x.spec.manW
     val exBias = x.spec.exBias
 
-    val order     = ts(0).nOrder
-    val adrW      = ts(0).adrW
-    val fracW     = ts(0).bp
+    val order     = t.nOrder
+    val adrW      = t.adrW
+    val fracW     = t.bp
     val extraBits = fracW - manW
 
 //     println(f"x = ${x.sgn}|${x.ex}(${x.ex-exBias})|${x.man.toLong.toBinaryString}")
@@ -63,89 +66,61 @@ object ATan2Stage2Sim {
     }
 
     // ------------------------------------------------------------------------
-    // atan table
-    val linearThreshold = calcLinearThreshold(manW)
+    // atan(x)/x table
 
-//     println(f"sim: xex = ${x.ex}")
-//     println(f"sim: linearThreshold = ${linearThreshold}")
+    val xShift0 = (exBias - x.ex)
+    val xShift  = Seq(xShift0, manW+1).min
+    val xFixed  = x.manW1 >> xShift
 
-    val (atanEx, atanMan) = if(x.ex == 0) {
-//       println("atan2Stage2Sim: Less than zero-threshold")
-      (x.ex, SafeLong(0))
-    } else if(x.ex < linearThreshold + exBias) {
-//       println("atan2Stage2Sim: Less than linear-threshold")
-      // linear approx
-      (x.ex, x.man)
+    // The case where x == 1, that means that x == y in atan2(x,y), corresponds
+    // to the 5- or 6-th special case.
+    assert(0 < xShift0, f"xShift = ${xShift}")
+
+    val (resManW1, resEx) = if(x.ex <= exBias + calcLinearThreshold(manW)) {
+
+      // without this check, polynomial become larger than (not equal to) 1...
+      (SafeLong(1) << fracW, 0)
+
     } else {
-      // table interpolation
-      val exadr = (-(x.ex - exBias) - 1).toInt
-      val t     = ts(exadr)
 
-      val (zEx0, zMan0) = if (t.nOrder == 0) {
-
-        val adr   = x.man
-        val res0  = t.interval(adr.toInt).eval(0L, 0)
-
-        val scaling = (-(x.ex - exBias) - 1).toInt
-        val shift   = fracW+1 - binaryWidthL(res0)
-        val res     = res0 << shift
-
-        (-shift - scaling, SafeLong(res))
-
+      val res0 = if (t.nOrder == 0) {
+        val adr   = xFixed
+        t.interval(adr.toInt).eval(0L, 0)
       } else {
         val dxbp = manW - adrW - 1
-        val d    = slice(0, dxbp+1, x.man) - (SafeLong(1)<<dxbp)
-        val adr  = slice(dxbp+1, adrW, x.man).toInt
-        val res0 = t.interval(adr).eval(d.toLong, dxbp)
-        assert((SafeLong(1)<<(fracW-2)) <= res0 && res0 < (SafeLong(1)<<fracW))
-
-        val res0MoreThanHalf = bit(fracW-1, res0) == 1
-        val shift  = if(res0MoreThanHalf) {1} else {2}
-
-//         println(f"atan2Stage2Sim:              2         1         0")
-//         println(f"atan2Stage2Sim:          4321098765432109876543210")
-//         println(f"atan2Stage2Sim: res0   = ${res0.toLong.toBinaryString}")
-//         println(f"atan2Stage2Sim: res>1/2= ${res0MoreThanHalf}")
-//         println(f"atan2Stage2Sim: fracW  = ${fracW}")
-//         println(f"atan2Stage2Sim: shift  = ${shift}")
-//         println(f"atan2Stage2Sim: x.ex   = ${x.ex - exBias}")
-//         println(f"atan2Stage2Sim: -x.ex-1= ${-(x.ex - exBias) - 1}")
-//         println(f"atan2Stage2Sim: zex0   = ${-(x.ex - exBias) - 1 - shift}")
-
-        val xexNobias = x.ex - exBias
-        val zex0 = xexNobias + 1 - shift
-        val res  = SafeLong(res0) << shift // normalize
-
-//         println(f"atan2Stage2Sim:               2         1         0")
-//         println(f"atan2Stage2Sim:          54321098765432109876543210")
-//         println(f"atan2Stage2Sim: res    = ${res.toLong.toBinaryString}")
-
-        assert((SafeLong(1)<<fracW) <= res && res <= (SafeLong(1)<<(fracW+1)))
-        (zex0, res)
+        val d    = slice(0, dxbp+1, xFixed) - (SafeLong(1)<<dxbp)
+        val adr  = slice(dxbp+1, adrW, xFixed).toInt
+        t.interval(adr).eval(d.toLong, dxbp)
       }
 
-      // round the result from table
-      val zMan = if (extraBits > 0) {
-        (zMan0 >> extraBits) + bit(extraBits-1, zMan0)
+      assert(res0 <= (SafeLong(1) << fracW),
+             f"xFixed = ${xFixed}, res0 = ${res0} > (1<<fracW) = ${SafeLong(1)<<fracW}")
+
+      if(res0 == (SafeLong(1) << fracW)) {
+        (SafeLong(1) << fracW, 0)
       } else {
-        zMan0
+        (SafeLong(res0) << 1, -1)
       }
-
-//       println(f"atan2Stage2Sim:             2         1         0")
-//       println(f"atan2Stage2Sim:          321098765432109876543210")
-//       println(f"atan2Stage2Sim: zMan   = ${zMan.toLong.toBinaryString}")
-
-      assert((1<<manW) <= zMan)
-
-      (zEx0+exBias, zMan - (SafeLong(1)<<manW))
     }
-    assert(0 <= atanMan && atanMan <= (SafeLong(1)<<manW))
+
+    // ------------------------------------------------------------------------
+    // calc x * atan(x)/x
+    //          ^^^^^^^^^ this term is calculated by polynomial
+
+    val atanProd = resManW1 * x.manW1
+    val atanProdMoreThan2 = bit((fracW+1)+(manW+1)-1, atanProd).toInt
+    val atanRound = slice(fracW + atanProdMoreThan2, manW, atanProd) +
+                    bit(fracW + atanProdMoreThan2 - 1, atanProd)
+    val atanRoundMoreThan2 = bit(manW, atanRound).toInt
+    val atanMan = slice(0, manW, atanRound)
+    val atanEx  = x.ex + resEx + atanProdMoreThan2 + atanRoundMoreThan2
 
 //     println(f"atan2Stage2Sim: atanEx  = ${atanEx }(${atanEx - exBias})")
 //     println(f"atan2Stage2Sim: atanMan = ${atanMan.toLong.toBinaryString}(${atanMan})")
 //     println(f"atan2Stage2Sim: atan(x) = sim(${new RealGeneric(x.spec, ysgn, atanEx, atanMan).toDouble}) == ref(${atan(x.toDouble)})")
 //     println(f"atan2Stage2Sim: status  = ${status}")
 
+    // ========================================================================
     // z correction by:
     //   ysgn *   atan(|y|/|x|)      .. x>0, |x|>|y| : 0
     //   ysgn * (-atan(|y|/|x|)+pi)  .. x<0, |x|>|y| : 1
@@ -232,7 +207,7 @@ object ATan2Stage2Sim {
   }
 
   def calcLinearThreshold(manW: Int): Int = {
-    -math.round(math.ceil(manW / 2.0 + 1.0)).toInt
+    -math.ceil(manW / 2.0 + 1.0).toInt
   }
 
   // number of tables depending on the exponent and linearThreshold
@@ -245,41 +220,17 @@ object ATan2Stage2Sim {
       calcWidthSetting: Option[Seq[Int]] = None,
       cbitSetting: Option[Seq[Int]] = None
     ) = {
-
-    // atan(x) = x - x^3/3 + x^5/5 + O(x^7)
-    //
-    // If x is in [0, 1],
-    //                  x/2 < atan(x)          < x
-    //         2^ex-1 * 1.m < atan(x)          < 2^ex * 1.m
-    //   1/4 < 2^-2   * 1.m < 2^(-ex-1)atan(x) < 1.m / 2 < 1
-    //        0.01 ==_2 1/4 < 2^(-ex-1)atan(x) < 1
-    //                        ~~~~~~~~~~~~~~~~
-    //                        this is calculated in polynomial
-    //
-    // So the result may take 0.010000 ~ 0.111111.
-
-    val linearThreshold = calcLinearThreshold(manW)
-
-    val nOrder = if (adrW >= manW) { 0 } else { order }
-
-    val maxCalcWidth = (-1 to linearThreshold by -1).map(ex => {
-        val tableD = new FuncTableDouble(
-          x => scalb(atan(scalb(1.0 + x, ex)), -ex-1),
-          nOrder)
-        tableD.addRange(0.0, 1.0, 1<<adrW)
-        val tableI = new FuncTableInt( tableD, fracW, calcWidthSetting, cbitSetting )
-        tableI.calcWidth
-      }).reduce( (lhs, rhs) => {
-        lhs.zip(rhs).map( x => max(x._1, x._2))
-      })
-
-    (-1 to linearThreshold by -1).map( ex => {
-      val tableD = new FuncTableDouble(
-        x => scalb(atan(scalb(1.0 + x, ex)), -ex-1),
-        nOrder)
-      tableD.addRange(0.0, 1.0, 1<<adrW)
-      new FuncTableInt( tableD, fracW, Some(maxCalcWidth), cbitSetting )
-    })
+    val tableD = new FuncTableDouble( x => {
+      val res = if(x == 0) {
+        1.0
+      } else {
+        Seq(atan(x) / x, 1.0).min
+      }
+      assert(0.0 < res && res <= 1) // pi/4 ~ 1
+      res
+    }, order)
+    tableD.addRange(0.0, 1.0, 1<<adrW)
+    new FuncTableInt(tableD, fracW, calcWidthSetting, cbitSetting)
   }
 
 }
