83064a135944ab9988d798b66a7fb94ea69ca267
ToruNiina <niina.toru.68u@gmail.com>
2024-04-23 19:32:46 +0900

    feat: enable to drive pipe in Threefry4_32 by en

--- src/main/scala/rng/threefry.scala
+++ src/main/scala/rng/threefry.scala
@@ -13,77 +13,119 @@ import chisel3.util._
 // Fully pipelined implementation, no loop
 // r : Number of rotations
 // rotStage : Insert pipeline stage every rotStage rotation
-class Threefry4_32( r: Int = 20, rotStage: Int = 0 ) extends Module {
+class Threefry4_32(
+  r: Int = 20,
+  rotStage: Int = 0
+) extends Module {
 
   def getParam = { (r, rotStage) }
+  def rotL( x : UInt, n : Int ) = { Cat( x.tail(n), x.head(n) ) }
 
-  def rotL( x : UInt, n : Int ) = {
-    Cat( x(x.getWidth-n-1,0), x(x.getWidth-1,x.getWidth-n) )
+  def nStages = {
+    if(rotStage == 0) {
+      0
+    } else {
+      Seq.tabulate(r)(j => {
+        if( ((j+1) % rotStage) == 0 ) { 1 } else { 0 }
+      }).fold(0)(_+_)
+    }
   }
 
-  //val Threefish_C240 = 0x1BD11BDAA9FC1A22
-  val C240 = 0x1BD11BDA.U
-
-  // Rotation constant
-  val R0 = Array[Int]( 10, 11, 13, 23, 6, 17, 25, 18 )
-  val R1 = Array[Int]( 26, 21, 27, 5, 20, 11, 10, 20 )
+  // -------------------------------------------------------------------------
 
   val io = IO(new Bundle {
-    val key   = Input(Vec(4, UInt(32.W)))
-    val count = Input(Vec(4, UInt(32.W)))
-    val rand  = Output(Vec(4, UInt(32.W)))
+    val en    = Input(Bool())
+    val input = new Bundle {
+      val valid = Input(Bool())
+      val key   = Input(Vec(4, UInt(32.W)))
+      val count = Input(Vec(4, UInt(32.W)))
+    }
+    val output = new Bundle {
+      val valid = Output(Bool())
+      val rand  = Output(Vec(4, UInt(32.W)))
+    }
   })
 
-  val keys = Wire(Vec(5, UInt(32.W)))
-  val X = Wire(Vec(r+1, Vec(4, UInt(32.W))))
-  val Xnext = Wire(Vec(r, Vec(4, UInt(32.W))))
+  val keys  = WireDefault(VecInit.fill(5  )(0.U(32.W)))
+  val x     = WireDefault(VecInit.fill(r+1)(VecInit.fill(4)(0.U(32.W))))
+  val xNext = WireDefault(VecInit.fill(r  )(VecInit.fill(4)(0.U(32.W))))
 
-  for(i <- 0 to 3) {
-    keys(i) := io.key(i)
-    X(0)(i) := io.count(i) + io.key(i)
+  // -------------------------------------------------------------------------
+  // init key
+
+  //val Threefish_C240 = 0x1BD11BDAA9FC1A22
+  val c240 = 0x1BD11BDA.U
+
+  for(i <- 0 until 4) {
+    keys(i) := io.input.key(i)
+  }
+  keys(4) := io.input.key.foldLeft(c240)( (z,k) => z ^ k )
+
+  // -------------------------------------------------------------------------
+  // set xNext
+
+  // Rotation constant
+  val r0 = Array[Int]( 10, 11, 13, 23, 6, 17, 25, 18 )
+  val r1 = Array[Int]( 26, 21, 27, 5, 20, 11, 10, 20 )
+
+  for(j <- 0 until r) {
+    if (j % 2 == 0) {
+      xNext(j)(0) :=       x(j)(0)              + x    (j)(1)
+      xNext(j)(1) := rotL( x(j)(1), r0(j % 8) ) ^ xNext(j)(0)
+      xNext(j)(2) :=       x(j)(2)              + x    (j)(3)
+      xNext(j)(3) := rotL( x(j)(3), r1(j % 8) ) ^ xNext(j)(2)
+    } else {
+      xNext(j)(0) :=       x(j)(0)              + x    (j)(3)
+      xNext(j)(1) := rotL( x(j)(1), r1(j % 8) ) ^ xNext(j)(2)
+      xNext(j)(2) :=       x(j)(2)              + x    (j)(1)
+      xNext(j)(3) := rotL( x(j)(3), r0(j % 8) ) ^ xNext(j)(0)
+    }
   }
 
-  keys(4) := (0 to 3).toSeq.foldLeft(C240)( (z,n) => z ^ io.key(n) )
+  // -------------------------------------------------------------------------
+  // set x
 
-  for(j <- 0 to r-1) {
-    val keyR = (j/4) + 1
-    if (j%2 == 0) {
-      Xnext(j)(0) := X(j)(0) + X(j)(1)
-      Xnext(j)(1) := rotL( X(j)(1), R0(j%8) ) ^ Xnext(j)(0)
-      Xnext(j)(2) := X(j)(2) + X(j)(3)
-      Xnext(j)(3) := rotL( X(j)(3), R1(j%8) ) ^ Xnext(j)(2)
+  val v = WireDefault(VecInit.fill(r+1)(false.B))
+  v(0) := io.input.valid
+
+  for(i <- 0 until 4) {
+    x(0)(i) := io.input.count(i) + io.input.key(i)
+  }
+
+  for(j <- 0 until r) {
+
+    val xj = WireDefault(VecInit.fill(4)(0.U(32.W)))
+    if (j % 4 == 3) {
+      val keyR = (j/4) + 1
+      xj(0) := xNext(j)(0) + keys((keyR+0)%5)
+      xj(1) := xNext(j)(1) + keys((keyR+1)%5)
+      xj(2) := xNext(j)(2) + keys((keyR+2)%5)
+      xj(3) := xNext(j)(3) + keys((keyR+3)%5) + keyR.U
     } else {
-      Xnext(j)(0) := X(j)(0) + X(j)(3)
-      Xnext(j)(3) := rotL( X(j)(3), R0(j%8) ) ^ Xnext(j)(0)
-      Xnext(j)(2) := X(j)(2) + X(j)(1)
-      Xnext(j)(1) := rotL( X(j)(1), R1(j%8) ) ^ Xnext(j)(2)
+      xj    := xNext(j)
     }
-    if ( (rotStage == 0) || ( ((j+1)%rotStage) != 0 ) ) { // No staging
-      if (j%4 == 3) {
-        X(j+1)(0) := Xnext(j)(0) + keys((keyR+0)%5)
-        X(j+1)(3) := Xnext(j)(3) + keys((keyR+3)%5) + keyR.U
-        X(j+1)(2) := Xnext(j)(2) + keys((keyR+2)%5)
-        X(j+1)(1) := Xnext(j)(1) + keys((keyR+1)%5)
-      } else {
-        X(j+1) := Xnext(j)
+
+    val addStage = !((rotStage == 0) || ( ((j+1) % rotStage) != 0 ))
+    if(addStage) {
+      val vReg = RegInit(false.B)
+      val xReg = RegInit(VecInit.fill(4)(0.U(32.W)))
+      when(io.en) {
+        xReg := xj
+        vReg := v(j)
       }
+      x(j+1) := xReg
+      v(j+1) := vReg
     } else {
-      if (j%4 == 3) {
-        X(j+1)(0) := RegNext(Xnext(j)(0) + keys((keyR+0)%5))
-        X(j+1)(3) := RegNext(Xnext(j)(3) + keys((keyR+3)%5) + keyR.U)
-        X(j+1)(2) := RegNext(Xnext(j)(2) + keys((keyR+2)%5))
-        X(j+1)(1) := RegNext(Xnext(j)(1) + keys((keyR+1)%5))
-      } else {
-        X(j+1) := RegNext(Xnext(j))
-      }
+      x(j+1) := xj
+      v(j+1) := v(j)
     }
   }
 
   //for(j <- 0 to r) {
   //printf("%d %x %x %x %x %x\n", j.U, X(j)(0), X(j)(1), X(j)(2), X(j)(3), rotL( X(j)(1), R0(j%8) ))
   //}
-  io.rand := X(r)
-
+  io.output.valid := v(r)
+  io.output.rand  := x(r)
 }
 
 object Threefry4_32Driver extends App {
