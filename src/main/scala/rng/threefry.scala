00db31a74491ef71dfd774bfcd263f73797bb8bb
ToruNiina <niina.toru.68u@gmail.com>
2024-04-24 13:51:38 +0900

    feat: add ready-valid I/F for Threefry4x32

--- src/main/scala/rng/threefry.scala
+++ src/main/scala/rng/threefry.scala
@@ -10,6 +10,77 @@ import scala.language.reflectiveCalls
 import chisel3._
 import chisel3.util._
 
+//
+// take random number from Threefry using ready/valid interface
+//
+class Threefry4x32Generator(
+  r: Int = 20,
+  rotStage: Int = 0,
+) extends Module {
+
+  val io = IO(new Bundle {
+    val initialized = Output(Bool())
+    val init = new Bundle {
+      val en    = Input(Bool())
+      val key   = Input(Vec(4, UInt(32.W)))
+      val count = Input(Vec(4, UInt(32.W)))
+    }
+    val rand = Decoupled(Vec(4, UInt(32.W)))
+  })
+
+  val rng = Module(new Threefry4_32(r, rotStage))
+
+  def getParam = { rng.getParam }
+  def nStages  = { rng.nStages  }
+
+  val initialized = RegInit(false.B)
+  io.initialized := initialized
+
+  val key   = RegInit(VecInit.fill(4)(0.U(32.W)))
+  val count = RegInit(VecInit.fill(4)(0.U(32.W)))
+  when(io.init.en) {
+    key   := io.init.key
+    count := io.init.count
+    initialized := true.B
+  }
+
+  val outQ = Module(new Queue(Vec(4, UInt(32.W)), 1, pipe=true, flow=false))
+  io.rand <> outQ.io.deq
+
+  val step = initialized && outQ.io.enq.ready
+
+  rng.io.en          := step
+  rng.io.input.valid := initialized
+  rng.io.input.key   := key
+  rng.io.input.count := count
+
+  // update counter only when we need to push pipeline forward
+  val countNext = WireDefault(VecInit(count.map(c => c + 1.U)))
+  when(step) {
+    count(0) := countNext(0)
+    when(countNext(0) === 0.U) {
+      count(1) := countNext(1)
+      when(countNext(1) === 0.U) {
+        count(2) := countNext(2)
+        when(countNext(2) === 0.U) {
+          count(3) := countNext(3)
+        }
+      }
+    }
+  }
+  outQ.io.enq.valid := rng.io.output.valid
+  outQ.io.enq.bits  := rng.io.output.rand
+
+  // printf("Threefry4x32Gen: init = %b, out.ready = %b, out.valid = %b, step = %b\n",
+  //   initialized, io.rand.ready, io.rand.valid, step)
+  // printf("Threefry4x32Gen: rng.en = %b, input.valid = %b, output.valid = %b\n",
+  //   rng.io.en, rng.io.input.valid, rng.io.output.valid)
+  // printf("Threefry4x32Gen: key = [%x,%x,%x,%x], cnt = [%x,%x,%x,%x]\n",
+  //   key(0), key(1), key(2), key(3), count(0), count(1), count(2), count(3))
+  // printf("Threefry4x32Gen: out = [%x,%x,%x,%x]\n",
+  //   rng.io.output.rand(0), rng.io.output.rand(1), rng.io.output.rand(2), rng.io.output.rand(3))
+}
+
 // Fully pipelined implementation, no loop
 // r : Number of rotations
 // rotStage : Insert pipeline stage every rotStage rotation
